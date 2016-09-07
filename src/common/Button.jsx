0fe1d858d37c07ba70a3d47153679d3aa34095aa
Øyvind Marthinsen <oma@knowit.no>
2016-09-07 11:06:41 +0200

    Add (c) header

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -1,6 +1,13 @@
-import React, { PropTypes, Component } from 'react';
-import classNames from 'classnames';
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
 
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
 
 const Button = ({ className, submit, loading, ...rest }) => {
   const classes = classNames(
