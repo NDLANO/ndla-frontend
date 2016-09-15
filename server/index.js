b119c6b60456873209d85ca5174e5766936fca6a
Øyvind Marthinsen <oma@knowit.no>
2016-09-15 15:41:42 +0200

    Rename server files

---
+++ server/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+/* eslint-disable */
+
+var http = require('http');
+
+require('babel-register');
+require('babel-polyfill');
+
+var config = require('../src/config')
+
+global.__CLIENT__ = false;
+global.__SERVER__ = true;
+global.__DISABLE_SSR__ = false;  // Disables server side rendering
+
+var serverConfig = require('./server');
+
+var server = http.createServer(serverConfig);
+
+server.listen(config.port);
+server.on('listening', () => {
+  console.log('Listening on ' + config.port);
+});
+/* eslint-enable */
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 /**
  * Copyright (c) 2016-present, NDLA.
  *
@@ -7,25 +6,94 @@
  *
  */
 
-/* eslint-disable */
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import express from 'express';
+import compression from 'compression';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { match, RouterContext } from 'react-router';
 
-var http = require('http');
+import enableDevMiddleWare from './enableDevMiddleware';
+import getConditionalClassnames from './getConditionalClassnames';
+import createMemoryHistory from './createMemoryHistory';
+import configureRoutes from '../src/routes';
+import configureStore from '../src/configureStore';
+import rootSaga from '../src/sagas';
 
-require('babel-register');
-require('babel-polyfill');
+import { configureLocale, isValidLocale } from '../src/containers/Locale/configureLocale';
+import Html from './Html';
 
-var config = require('../src/config')
+const app = express();
 
-global.__CLIENT__ = false;
-global.__SERVER__ = true;
-global.__DISABLE_SSR__ = false;  // Disables server side rendering
+if (process.env.NODE_ENV === 'development') {
+  enableDevMiddleWare(app);
+}
 
-var app = require('./app');
+app.use(compression());
+app.use(express.static('htdocs', {
+  maxAge: 1000 * 60 * 60 * 24 * 365, // One year
+}));
 
-var server = http.createServer(app);
+const renderHtmlString = (locale, userAgentString, state = {}, component = undefined) =>
+  renderToString(<Html lang={locale} state={state} component={component} className={getConditionalClassnames(userAgentString)} />);
 
-server.listen(config.port);
-server.on('listening', () => {
-  console.log('Listening on ' + config.port);
+app.get('*', (req, res) => {
+  const paths = req.url.split('/');
+  const { currentLocale: locale } = configureLocale(paths[1]);
+  const userAgentString = req.headers['user-agent'];
+
+
+  if (global.__DISABLE_SSR__) { // eslint-disable-line no-underscore-dangle
+    const htmlString = renderHtmlString(locale, userAgentString);
+    res.send(`<!doctype html>\n${htmlString}`);
+    return;
+  }
+
+  const options = isValidLocale(paths[1]) ? { basename: `/${locale}/` } : {};
+  const location = !options.basename ? req.url : req.url.replace(`${locale}/`, '');
+  const memoryHistory = createMemoryHistory(req.url, options);
+
+  const store = configureStore({ locale }, memoryHistory);
+
+  const history = syncHistoryWithStore(memoryHistory, store);
+
+  match({ history, routes: configureRoutes(store), basename: `/${locale}`, location }, (err, redirectLocation, props) => {
+    if (err) {
+      // something went badly wrong, so 500 with a message
+      res.status(500).send(err.message);
+    } else if (redirectLocation) {
+      // we matched a ReactRouter redirect, so redirect from the server
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+    } else if (props) {
+      // if we got props, that means we found a valid component to render for the given route
+      const component =
+        (<Provider store={store}>
+          <RouterContext {...props} />
+        </Provider>);
+
+      store.runSaga(rootSaga).done
+        .then(() => {
+          const state = store.getState();
+          const htmlString = renderHtmlString(locale, userAgentString, state, component);
+          const status = props.routes.find(r => r.status === 404) !== undefined ? 404 : 200;
+          res.status(status).send(`<!doctype html>\n${htmlString}`);
+        })
+        .catch((error => {
+          res.status(500).send(error.message);
+        }));
+
+      // Trigger sagas for components by rendering them (should not have any performance implications)
+			// https://github.com/yelouafi/redux-saga/issues/255#issuecomment-210275959
+      renderToString(component);
+
+			// Dispatch a close event so sagas stop listening after they have resolved
+      store.close();
+    } else {
+      res.sendStatus(500);
+    }
+  });
+  return;
 });
-/* eslint-enable */
+
+module.exports = app;
