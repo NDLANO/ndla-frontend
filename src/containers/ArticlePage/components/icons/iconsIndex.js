ff78adcddcd1ce1b7e135e3dfaa10c0f1577e883
kmelve <knut@netliferesearch.com>
2016-10-06 07:54:01 +0200

    add basic lisence info and meta information

---
+++ src/containers/ArticlePage/components/icons/iconsIndex.js
@@ -0,0 +1,6 @@
+'use-strict'
+export Byncnd from './by-nc-nd.svg'
+export Byncsa from './by-nc-sa.svg'
+export Bync from './by-nc.svg'
+export Bynd from './by-nd.svg'
+export Bysa from './by-sa.svg'
