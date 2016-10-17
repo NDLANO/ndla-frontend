094f5ca31026bc6616a92b04cf2a49305fc115a0
kmelve <knut@netliferesearch.com>
2016-10-17 14:27:54 +0200

    delete files

--- src/containers/ArticlePage/components/icons/iconsIndex.js
+++
@@ -1,6 +0,0 @@
-'use-strict'
-export Byncnd from './by-nc-nd.svg'
-export Byncsa from './by-nc-sa.svg'
-export Bync from './by-nc.svg'
-export Bynd from './by-nd.svg'
-export Bysa from './by-sa.svg'
