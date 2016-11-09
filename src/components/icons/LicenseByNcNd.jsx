aedbad134c254b4afc8c2a19302341c094e2374d
Øyvind Marthinsen <oma@knowit.no>
2016-11-09 08:11:09 +0100

    Add missing (c) headers

--- src/components/icons/LicenseByNcNd.jsx
+++ src/components/icons/LicenseByNcNd.jsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import React from 'react';
 
 const LicenseByNcNd = ({ className }) => (
