eaf35ca8be0797295d8b2fd8432e4ade503b47f3
kmelve <knut@netliferesearch.com>
2016-11-24 08:58:27 +0100

    reorder elements

--- src/components/license/ArticleLicenseInfo.jsx
+++ src/components/license/ArticleLicenseInfo.jsx
@@ -12,23 +12,24 @@ import { injectT } from '../../i18n';
 const ArticleLicenseInfo = ({ t, license, authors, title, created, updated, icons }) => (
   <div>
     <div>
+      <div>
+        {icons}
+        <h3 className="license__heading">{license.title}</h3>
+        <p>{license.description}</p>
+      </div>
       <div className="license__publication-info c-bodybox">
         <ul className="license__list">
-          <li className="license__list-item">Tittel: {title}<br /></li>
-          <li className="license__list-item">{`${t('article.created')} ${created}. ${t('article.lastUpdated')} ${updated}`}</li>
+          <li className="license__list-item"><h4>Tittel:</h4> {title}<br /></li>
+          <li className="license__list-item"><h4>{t('article.created')}:</h4> {`${created}. (${t('article.lastUpdated')} ${updated}`})</li>
         </ul>
         <ul className="license__list">
-          { t('license.creators', { num: authors.length })}
+          <h4>{ t('license.creators', { num: authors.length })}</h4>
           {
             authors.map((author, i) => (<li className="license__list-item" key={i}>{author.name} {author.type ? `(${author.type})` : ''}</li>))
           }
         </ul>
       </div>
     </div>
-    {icons}
-    <h3>{license.title}</h3>
-    <p>{license.description}</p>
-
   </div>
 );
 
