f922afa4f1391664ced7360b2676d12f18c40aa1
Øyvind Marthinsen <oma@knowit.no>
2016-11-29 14:54:10 +0100

    Add subject menu to masthead

---
+++ src/containers/App/components/SubjectsMenu.jsx
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import injectSubjects from '../../SubjectMenu/injectSubjects';
+
+const SubjectsMenu = ({ subjects }) => (
+  <ul className="subject-menu">
+    { subjects.map(subject =>
+      (<li className="subject-menu--item" key={subject.id}>
+        <Link className="subject-menu--link" to={`/subject/${subject.id}`}>{ subject.name }</Link>
+      </li>)
+    ) }
+  </ul>
+);
+
+SubjectsMenu.propTypes = {
+  subjects: PropTypes.array.isRequired,
+};
+
+export default injectSubjects(SubjectsMenu);
