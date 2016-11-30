c56d003dd8850c26d3204769dac6dfdff0b37c6c
Øyvind Marthinsen <oma@knowit.no>
2016-11-30 11:43:46 +0100

    Move subject files to SubjectPage folder

--- src/containers/App/components/SubjectsMenu.jsx
+++ src/containers/App/components/SubjectsMenu.jsx
@@ -8,7 +8,7 @@
 
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import { injectSubjects } from '../../SubjectsPage/subjectHOCs';
+import { injectSubjects } from '../../SubjectPage/subjectHOCs';
 
 const SubjectsMenu = ({ subjects }) => (
   <ul className="subject-menu">
