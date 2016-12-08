179ee14ba9062f72cccd67ffed71e2ea5f77249d
Øyvind Marthinsen <oma@knowit.no>
2016-12-08 09:53:22 +0100

    Refactor LicenseBox to a stateless component

--- src/components/license/LicenseBox.jsx
+++ src/components/license/LicenseBox.jsx
@@ -6,7 +6,7 @@
  *
  */
 
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import defined from 'defined';
 import { injectT } from '../../i18n';
 import ImageLicenseList from './ImageLicenseList';
@@ -18,56 +18,43 @@ import Tabs from './Tabs';
 import { ArticleShape } from '../../shapes';
 
 
-class LicenseBox extends Component {
+function buildLicenseTabList(article, license, locale, t) {
+  const images = defined(article.contentCopyrights.image, []);
+  const audios = defined(article.contentCopyrights.audio, []);
 
-  buildLicenseTabList() {
-    const { article, license, locale, t } = this.props;
+  const tabs = [];
 
-    const images = defined(article.contentCopyrights.image, []);
-    const audios = defined(article.contentCopyrights.audio, []);
-
-    const tabs = [];
-
-    if (images.length > 0) {
-      tabs.push({ key: 'images', displayName: t('license.tabs.images'), content: <ImageLicenseList images={images} heading={t('license.heading')} locale={locale} /> });
-    }
-
-    if (article) {
-      tabs.push({
-        key: 'article',
-        displayName: t('license.tabs.article'),
-        content: (<ArticleLicenseInfo article={article} icons={this.props.children} license={license} />),
-      });
-    }
-
-    if (audios.length > 0) {
-      tabs.push({
-        key: 'audios',
-        displayName: t('license.tabs.audios'),
-        content: <AudioLicenseList audios={audios} heading={t('license.heading')} locale={locale} />,
-      });
-    }
-
-    tabs.push({ key: 'text', displayName: t('license.tabs.text'), content: <TextDownloadList /> });
-    tabs.push({ key: 'cite', displayName: t('license.tabs.cite'), content: <Citation article={article} /> });
-
-    return tabs;
+  if (images.length > 0) {
+    tabs.push({ key: 'images', displayName: t('license.tabs.images'), content: <ImageLicenseList images={images} heading={t('license.heading')} locale={locale} /> });
   }
 
-  render() {
-    const { article, t } = this.props;
-
-    const contentType = article.contentType.toLowerCase();
-    const tabs = this.buildLicenseTabList();
-    return (
-      <div>
-        <h1 className="license__heading">{t('license.tabs.heading', { contentType })}</h1>
-        <p className="license__introduction">{t('license.tabs.introduction', { contentType })}</p>
-        <Tabs tabs={tabs} />
-      </div>
-    );
+  if (article) {
+    tabs.push({ key: 'article', displayName: t('license.tabs.article'), content: (<ArticleLicenseInfo article={article} icons={this.props.children} license={license} />) });
   }
+
+  if (audios.length > 0) {
+    tabs.push({ key: 'audios', displayName: t('license.tabs.audios'), content: <AudioLicenseList audios={audios} heading={t('license.heading')} locale={locale} /> });
+  }
+
+  tabs.push({ key: 'text', displayName: t('license.tabs.text'), content: <TextDownloadList /> });
+  tabs.push({ key: 'cite', displayName: t('license.tabs.cite'), content: <Citation article={article} /> });
+
+  return tabs;
 }
+
+
+const LicenseBox = ({ article, license, locale, t }) => {
+  const contentType = article.contentType.toLowerCase();
+  const tabs = buildLicenseTabList(article, license, locale, t);
+  return (
+    <div>
+      <h1 className="license__heading">{t('license.tabs.heading', { contentType })}</h1>
+      <p className="license__introduction">{t('license.tabs.introduction', { contentType })}</p>
+      <Tabs tabs={tabs} />
+    </div>
+  );
+};
+
 
 LicenseBox.propTypes = {
   license: PropTypes.object.isRequired,
