c74068cd705827dbf0df103f1b73875bd232917a
Øyvind Marthinsen <oma@knowit.no>
2016-12-16 13:15:56 +0100

    Include meta description field

--- src/containers/ArticlePage/articleSelectors.js
+++ src/containers/ArticlePage/articleSelectors.js
@@ -8,7 +8,7 @@
 
 import { createSelector } from 'reselect';
 import { getLocale } from '../Locale/localeSelectors';
-import { titleI18N } from '../../util/i18nFieldFinder';
+import { titleI18N, metaDescriptionI18N } from '../../util/i18nFieldFinder';
 import formatDate from '../../util/formatDate';
 
 const getArticleFromState = state => state.articles;
@@ -24,6 +24,7 @@ export const getArticle = articleId => createSelector(
     article ? {
       ...article,
       title: titleI18N(article, locale, true),
+      metaDescription: metaDescriptionI18N(article, locale, true),
       created: formatDate(article.created, locale),
       updated: formatDate(article.updated, locale),
     } : {}
