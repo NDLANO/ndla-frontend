b1f832ef9a5712300c762e1a55421e0a50ce06b5
Tanja Gruschke <tmg@knowit.no>
2017-06-12 16:42:30 +0200

    #401 using helmet for header security settings

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import express from 'express';
+import helmet from 'helmet';
 import compression from 'compression';
 import { Provider } from 'react-redux';
 import { IntlProvider } from 'react-intl';
@@ -40,22 +41,96 @@ app.use(
     maxAge: 1000 * 60 * 60 * 24 * 365, // One year
   }),
 );
-app.use((req, res, next) => {
-  res.setHeader(
-    'Strict-Transport-Security',
-    'max-age=31536000; includeSubDomains',
-  );
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'self' blob: ; " +
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.ndla.no https://players.brightcove.net http://players.brightcove.net https://players.brightcove.net https://www.nrk.no https://www.googletagmanager.com https://www.google-analytics.com https://www.youtube.com https://s.ytimg.com https://cdn.auth0.com https://vjs.zencdn.net https://httpsak-a.akamaihd.net *.brightcove.com  *.brightcove.net  bcove.me  bcove.video  *.api.brightcove.com  *.o.brightcove.com  players.brightcove.net  hls.ak.o.brightcove.com  uds.ak.o.brightcove.com  brightcove.vo.llnwd.net  *.llnw.net  *.llnwd.net  *.edgefcs.net  *.akafms.net  *.edgesuite.net  *.akamaihd.net  *.analytics.edgekey.net  *.deploy.static.akamaitechnologies.com  *.cloudfront.net  hlstoken-a.akamaihd.net  vjs.zencdn.net  *.gallerysites.net ; " +
-      "style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com https://fonts.gstatic.com; " +
-      "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com data: ; " +
-      "connect-src 'self' https://*.ndla.no https://logs-01.loggly.com https://edge.api.brightcove.com https://secure.brightcove.com https://bcsecure01-a.akamaihd.net https://hlsak-a.akamaihd.net ; " +
-      "img-src 'self' https://*.ndla.no https://www.google-analytics.com https://stats.g.doubleclick.net http://metrics.brightcove.com https://httpsak-a.akamaihd.net data: ;",
-  );
-  next();
-});
+
+app.use(
+  helmet({
+    hsts: {
+      maxAge: 31536000,
+      includeSubDomains: true,
+    },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'", 'blob:'],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          " 'unsafe-eval'",
+          'https://*.ndla.no',
+          'https://players.brightcove.net',
+          'http://players.brightcove.net',
+          'https://players.brightcove.net',
+          'https://www.nrk.no',
+          'https://www.googletagmanager.com',
+          'https://www.google-analytics.com',
+          'https://www.youtube.com',
+          'https://s.ytimg.com',
+          'https://cdn.auth0.com',
+          'https://vjs.zencdn.net',
+          'https://httpsak-a.akamaihd.net',
+          '*.brightcove.com',
+          '*.brightcove.net',
+          'bcove.me',
+          'bcove.video',
+          '*.api.brightcove.com',
+          '*.o.brightcove.com',
+          'players.brightcove.net',
+          'hls.ak.o.brightcove.com',
+          'uds.ak.o.brightcove.com',
+          'brightcove.vo.llnwd.net',
+          '*.llnw.net',
+          '*.llnwd.net',
+          '*.edgefcs.net',
+          '*.akafms.net',
+          '*.edgesuite.net',
+          '*.akamaihd.net',
+          '*.analytics.edgekey.net',
+          '*.deploy.static.akamaitechnologies.com',
+          '*.cloudfront.net',
+          'hlstoken-a.akamaihd.net',
+          'vjs.zencdn.net',
+          ' *.gallerysites.net',
+        ],
+        styleSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "'unsafe-eval'",
+          'https://fonts.googleapis.com',
+          'https://fonts.gstatic.com',
+        ],
+        fontSrc: [
+          "'self'",
+          'https://fonts.googleapis.com',
+          'https://fonts.gstatic.com',
+          'data:',
+        ],
+        imgSrc: [
+          " 'self' " +
+            ' https://*.ndla.no' +
+            ' https://www.google-analytics.com' +
+            ' https://stats.g.doubleclick.net' +
+            ' http://metrics.brightcove.com' +
+            ' https://httpsak-a.akamaihd.net' +
+            ' data:',
+        ],
+        connectSrc: [
+          " 'self' ",
+          'https://*.ndla.no',
+          'https://logs-01.loggly.com',
+          'https://edge.api.brightcove.com',
+          'https://secure.brightcove.com',
+          'https://bcsecure01-a.akamaihd.net',
+          'https://hlsak-a.akamaihd.net',
+        ],
+      },
+    },
+    frameguard: process.env.NODE_ENV === 'development'
+      ? {
+          action: 'allow-from',
+          domain: '*://localhost',
+        }
+      : undefined,
+  }),
+);
 
 const renderHtmlString = (
   locale,
