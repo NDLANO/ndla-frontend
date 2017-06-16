26b8aed556256258320b520916c6aa42457a6ad6
Øyvind Marthinsen <oma@knowit.no>
2017-06-16 15:33:41 +0200

    Follow latest license box changes

--- src/components/license/ImageLicenseList.jsx
+++ src/components/license/ImageLicenseList.jsx
@@ -10,42 +10,60 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { uuid } from 'ndla-util';
 import {
+  Button,
   MediaList,
   MediaListItem,
   MediaListItemImage,
   MediaListItemBody,
   MediaListItemActions,
-  MediaListItemMeta,
-} from './MediaList';
-import Icon from '../Icon';
+} from 'ndla-ui';
+import { MediaListItemMeta } from './MediaList';
 import { CopyrightObjectShape } from '../../shapes';
+
+const getSrcSets = image =>
+  [
+    `${image.src}?width=1440 1440w`,
+    `${image.src}?width=1120 1120w`,
+    `${image.src}?width=1000 1000w`,
+    `${image.src}?width=960 960w`,
+    `${image.src}?width=800 800w`,
+    `${image.src}?width=640 640w`,
+    `${image.src}?width=480 480w`,
+    `${image.src}?width=320 320w`,
+    `${image.src}?width=320 320w`,
+  ].join(', ');
 
 const ImageLicenseInfo = ({ image, locale }) =>
   <MediaListItem>
     <MediaListItemImage>
-      <img width="260" alt={image.altText} src={`${image.src}?width=260`} />
+      <img
+        alt={image.altText}
+        src={`${image.src}`}
+        srcSet={getSrcSets(image)}
+        sizes="(min-width: 800px) 360px, (min-width: 600px) 300px, 100vw"
+      />
     </MediaListItemImage>
     <MediaListItemBody
+      title="Regler for bruk av bildet:"
       license={image.copyright.license.license}
       locale={locale}>
       <MediaListItemActions>
-        <button
-          className="c-button c-button--small c-button--transparent"
-          type="button">
-          <Icon.Copy className="c-modal__button-icon" /> Kopier referanse
-        </button>
-        <a
-          href={image.src}
-          className="c-button c-button--small c-button--transparent">
-          <Icon.Link className="c-modal__button-icon" /> Gå til kilde
-        </a>
-        <button
-          className="c-button c-button--small c-button--transparent"
-          type="button">
-          <Icon.OpenWindow className="c-modal__button-icon" /> Vis bilde
-        </button>
+        <div className="c-medialist__ref">
+          <h3 className="c-medialist__title">
+            Slik skal du referere til bildet:
+          </h3>
+          <MediaListItemMeta authors={image.copyright.authors} />
+          <Button outline className="c-licenseToggle__button">
+            Kopier referanse
+          </Button>
+          <a
+            href={image.src}
+            className="c-button c-button--outline c-licenseToggle__button"
+            download>
+            Last ned
+          </a>
+        </div>
       </MediaListItemActions>
-      <MediaListItemMeta authors={image.copyright.authors} />
     </MediaListItemBody>
   </MediaListItem>;
 
