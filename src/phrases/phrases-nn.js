4a8b4a738428d8884cb0a4fb1edc12123ca2f122
Øyvind Marthinsen <oma@knowit.no>
2018-01-29 08:17:42 +0100

    Add phrases-nn file

---
+++ src/phrases/phrases-nn.js
@@ -0,0 +1,177 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+const phrases = {
+  articlePage: {
+    errorDescription:
+      'Vi beklager, men en feil oppsto under lasting av ressursen.',
+    error404Description:
+      'Vi beklager, men vi kunne ikke finne ressursen du leter etter.',
+  },
+  notFoundPage: {
+    errorDescription:
+      'Vi beklager, men vi fant ikke siden du prøvde å komme til.',
+  },
+  searchPage: {
+    noHits: 'Ingen artikler samsvarte med søket ditt på: {query}',
+  },
+  subjectPage: {
+    errorDescription:
+      'Vi beklager, men en feil oppsto under lasting av emnene.',
+    tabs: {
+      topics: 'Emner',
+    },
+  },
+  subjectsPage: {
+    errorDescription:
+      'Vi beklager, men en feil oppsto under lasting av fagene.',
+    chooseSubject: 'Velg fag',
+  },
+  topicPage: {
+    topicErrorDescription:
+      'Vi beklager, men en feil oppsto under lasting av emne innholdet.',
+    articleErrorDescription:
+      'Vi beklager, men en feil oppsto under lasting av emne beskrivelsen.',
+    topic: 'EMNE',
+    topics: 'Emner',
+  },
+  welcomePage: {
+    subjects: 'Fag',
+    search: 'Gå til søk',
+    errorDescription:
+      'Vi beklager, men en feil oppsto under lasting av fagene.',
+  },
+  meta: {
+    description:
+      'Kvalitetssikrede fritt tilgjengelige nettbaserte læremidler for videregående opplæring',
+  },
+  masthead: {
+    menu: {
+      close: 'Lukk',
+      goTo: 'Gå til',
+      search: 'Søk',
+      subjectOverview: 'Fagoversikt',
+      title: 'Meny',
+    },
+  },
+  logo: {
+    altText: 'Nasjonal digital læringsarena',
+  },
+  resource: {
+    errorDescription:
+      'Vi beklager, men en feil oppsto under lasting av emne ressurser.',
+    noCoreResourcesAvailable: 'Det er ikke noe kjernestoff tilgjengelig.',
+    activateAdditionalResources: 'Vis tilleggsstoff',
+    toggleFilterLabel: 'Tilleggsstoff',
+    showLess: 'Vis mindre',
+    showMore: 'Vis mer',
+  },
+  searchForm: {
+    placeholder: 'Søk etter artikler',
+    btn: 'Søk',
+    order: {
+      relevance: 'Relevans',
+      title: 'Alfabetisk',
+    },
+  },
+  article: {
+    lastUpdated: 'Sist oppdatert',
+    edition: 'Utgave',
+    publisher: 'Utgiver',
+    closeLicenseBox: 'Lukk boks',
+    openLicenseBox: 'Bruk artikkel',
+  },
+  subject: {
+    associatedTopics: 'Tilhørende emner',
+  },
+  license: {
+    heading: 'Slik gjenbruker du innhold',
+    learnMore: 'Lær mer om åpne lisenser',
+    copyTitle: 'Kopier referanse',
+    hasCopiedTitle: 'Kopiert!',
+    embed: 'Bygg inn',
+    embedCopied: 'Kopierte bygg inn kode!',
+    download: 'Last ned',
+    tabs: {
+      text: 'Tekst',
+      images: 'Bilder',
+      audio: 'Lyd',
+      video: 'Video',
+    },
+    images: {
+      heading: 'Slik bruker du bilder fra artikkelen',
+      description:
+        'Husk å kopier teksten som skal legges ved bildet der du bruker det.',
+      rules: 'Regler for bruk av bildet:',
+      source: 'Kilde',
+      title: 'Tittel',
+    },
+    text: {
+      heading: 'Slik bruker du tekst fra artikkelen',
+      description:
+        'Artikkelen kan være satt sammen av flere ulike tekster, som listes opp her.',
+      rules: 'Regler for bruk av teksten:',
+      published: 'Publiseringsdato',
+    },
+    audio: {
+      heading: 'Slik bruker du lydfiler',
+      description:
+        'Husk å kopier teksten som skal legges ved lydfilen der du bruker den.',
+      rules: 'Regler for bruk av lydfilen:',
+    },
+    video: {
+      heading: 'Slik bruker du video fra artikkelen',
+      description:
+        'Husk å kopier teksten som skal legges ved videoen der du bruker den.',
+      rules: 'Regler for bruk av videoen:',
+    },
+    creditType: {
+      originator: 'Opphavsmann',
+      photographer: 'Fotograf',
+      artist: 'Kunstner',
+      editorial: 'Redaksjonelt',
+      writer: 'Forfatter',
+      scriptwriter: 'Manusforfatter',
+      reader: 'Innleser',
+      translator: 'Oversetter',
+      director: 'Regissør',
+      illustrator: 'Illustratør',
+      cowriter: 'Medforfatter',
+      composer: 'Komponist',
+      processor: 'Bearbeider',
+      facilitator: 'Tilrettelegger',
+      linguistic: 'Språklig',
+      idea: 'Idé',
+      compiler: 'Sammenstiller',
+      correction: 'Korrektur',
+      rightsholder: 'Rettighetshaver',
+      publisher: 'Forlag',
+      distributor: 'Distributør',
+      supplier: 'Leverandør',
+    },
+  },
+  breadcrumb: {
+    subjectsLinkText: 'Fag',
+    label: 'Du er her:',
+  },
+  errorMessage: {
+    title: 'Oops, noe gikk galt',
+    description: 'Vi beklager, men en feil oppsto.',
+    back: 'Tilbake',
+    goToFrontPage: 'Gå til forsiden',
+  },
+  footer: {
+    aboutNDLA: 'Om NDLA',
+    selectLanguage: 'Velg språk (language): ',
+    footerInfo: 'Nettstedet er utarbeidet av NDLA med åpen kildekode.',
+    footerEditiorInChief: 'Ansvarlig redaktør: ',
+    footerManagingEditor: 'Utgaveansvarlig: ',
+  },
+};
+
+export default phrases;
