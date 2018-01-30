1b1821c6fd0526e53cbdcc2bb230a5fed3cc104b
Torgeir Haugen <torgeirhaugen@gmail.com>
2018-01-30 14:15:06 +0100

    FilterSaga test

---
+++ src/containers/Filters/__tests__/filterSaga-test.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { expectSaga } from 'redux-saga-test-plan';
+import nock from 'nock';
+import * as sagas from '../filterSagas';
+import { actions } from '../filter';
+
+const filters = [
+  {
+    id: 'test1',
+    name: 'test1',
+  },
+  {
+    id: 'test2',
+    name: 'test2',
+  },
+];
+
+test('filters are fetched', () => {
+  nock('http://ndla-api')
+    .get('/taxonomy/v1/subjects/urn:subject:1/filters')
+    .reply(200, filters);
+
+  return expectSaga(sagas.watchFetchFilters)
+    .put(actions.fetchSubjectFiltersSuccess({ id: 'urn:subject:1', filters }))
+    .dispatch(actions.fetchSubjectFilters('urn:subject:1'))
+    .run({ silenceTimeout: true });
+});
