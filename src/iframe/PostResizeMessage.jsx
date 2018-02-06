ac926bc6eaef9e0c98fcda70b1bbd8f3f0792906
Christian Peter <christian150790@gmail.com>
2018-02-06 15:24:33 +0100

    Adds width check

--- src/iframe/PostResizeMessage.jsx
+++ src/iframe/PostResizeMessage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 class PostResizeMessage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      width: 0,
+    };
+  }
   componentDidMount() {
     this.sendResizeToParentWindow();
-
     window.addEventListener('resize', this.sendResizeToParentWindow);
   }
 
@@ -12,13 +17,15 @@ class PostResizeMessage extends React.Component {
   }
 
   sendResizeToParentWindow = () => {
-    if (window.parent !== undefined) {
-      window.parent.postMessage(
-        {
-          event: 'resize',
-          height: document.body.scrollHeight,
-        },
-        '*',
+    if (window.parent !== undefined && this.state.width !== window.outerWidth) {
+      this.setState({ width: window.outerWidth }, () =>
+        window.parent.postMessage(
+          {
+            event: 'resize',
+            height: document.body.scrollHeight,
+          },
+          '*',
+        ),
       );
     }
   };
