8e6f0fa011a285348abe73f9fccef5ef299cc1a6
Christian Peter <christian150790@gmail.com>
2018-02-19 13:42:01 +0100

    Updates prod urls

--- src/config.js
+++ src/config.js
@@ -33,7 +33,7 @@ const ndlaFrontendDomain = () => {
     case 'local':
       return 'http://localhost:30017';
     case 'prod':
-      return 'https://ndla-frontend.api.ndla.no';
+      return 'https://beta.ndla.no';
     default:
       return `https://ndla-frontend.${ndlaEnvironment}.api.ndla.no`;
   }
@@ -44,7 +44,7 @@ const learningPathDomain = () => {
     case 'local':
       return 'http://localhost:30007';
     case 'prod':
-      return 'http://learningpath-frontend.api.ndla.no';
+      return 'http://beta.sti.ndla.no';
     default:
       return `http://learningpath-frontend.${ndlaEnvironment}.api.ndla.no`;
   }
