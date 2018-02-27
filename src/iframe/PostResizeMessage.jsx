0cf008b95480c0edcff47f35764dbb9edf9d1f1f
Christian Peter <christian150790@gmail.com>
2018-02-27 13:11:22 +0100

    Adds fix for resizer where height changes is also considered #908

--- src/iframe/PostResizeMessage.jsx
+++ src/iframe/PostResizeMessage.jsx
@@ -13,16 +13,19 @@ class PostResizeMessage extends React.Component {
     super();
     this.state = {
       width: 0,
+      height: 0,
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    this.onWatchHeight(true);
     this.onResizeReady();
     window.addEventListener('resize', this.onResizeReady);
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.onResizeReady);
+    this.onWatchHeight = null;
   }
 
   onResizeReady = () => {
@@ -37,23 +40,40 @@ class PostResizeMessage extends React.Component {
     }
   };
 
+  onWatchHeight = (initialLoad = false) => {
+    setTimeout(() => {
+      const container = document.querySelector('.c-article--iframe');
+      const height = container ? container.scrollHeight + 20 : 0;
+      if (this.state.height !== height && !initialLoad) {
+        this.resizer();
+      }
+      this.onWatchHeight(false);
+    }, 100);
+  };
+
   sendResizeToParentWindow = () => {
     const outerWidth =
       document && document.body
         ? document.body.getBoundingClientRect().width
         : 0;
-
     if (window.parent && this.state.width !== outerWidth) {
-      this.setState({ width: outerWidth }, () =>
-        window.parent.postMessage(
-          {
-            event: 'resize',
-            height: document.body.scrollHeight,
-          },
-          '*',
-        ),
-      );
+      this.resizer(outerWidth);
     }
+  };
+
+  resizer = (width = undefined) => {
+    const container = document.querySelector('.c-article--iframe');
+    const height = container ? container.scrollHeight + 20 : 0;
+    const newState = width !== undefined ? { width, height } : { height };
+    this.setState(newState, () =>
+      window.parent.postMessage(
+        {
+          event: 'resize',
+          height,
+        },
+        '*',
+      ),
+    );
   };
 
   render() {
