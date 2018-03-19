b95f926dedcb50ceed7c3300f92bf1fd79143896
Torgeir Haugen <torgeirhaugen@gmail.com>
2018-03-19 15:07:26 +0100

    Test that api calls happen

--- end-to-end-tests/integration/subjects_page_spec.js
+++ end-to-end-tests/integration/subjects_page_spec.js
@@ -1,9 +1,15 @@
 describe('Subjects page', () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.get('[data-cy=subject-list] li:first-child a').click();
+    cy.server();
+    const visitOptions = {
+      onBeforeLoad: win => {
+        win.fetch = null;
+      },
+    };
+    cy.visit('/', visitOptions);
   });
   it('should include a list of valid topic links', () => {
+    cy.get('[data-cy=subject-list] li:first-child a').click();
     cy.get('[data-cy="topic-list"] h1').contains(/\w+/);
     cy.get('[data-cy="topic-list"] a').each(el => {
       cy
@@ -14,6 +20,7 @@ describe('Subjects page', () => {
     });
   });
   it('should have a valid breadcrumb, filter and language select', () => {
+    cy.get('[data-cy=subject-list] li:first-child a').click();
     cy
       .get('[data-cy="breadcrumb-section"] a')
       .should('have.length', 1)
@@ -25,5 +32,20 @@ describe('Subjects page', () => {
     });
     cy.get('[data-cy=language-select]').select(['nn']);
     cy.url().should('include', 'nn');
+  });
+  it('Should call filter, taxonomi and article-api', () => {
+    cy.route('**/taxonomy/v1/subjects/**/filters').as('filterCall');
+    cy
+      .route('**/taxonomy/v1/subjects/**/topics/?recursive=true&language=nb')
+      .as('topicCall');
+    cy.route('**/article-api/v2/articles**').as('resourceCall');
+    cy.get('[data-cy=subject-list] li:first-child a').click();
+    cy
+      .wait(['@filterCall', '@topicCall', '@resourceCall'])
+      .spread((filter, topic, resource) => {
+        expect(topic.response.body).to.be.an('array');
+        expect(topic.response.body).to.be.an('array');
+        expect(resource.response.body).to.be.an('object');
+      });
   });
 });
--- end-to-end-tests/integration/topic_page_spec.js
+++ end-to-end-tests/integration/topic_page_spec.js
@@ -1,9 +1,39 @@
 describe('Topic page', () => {
   beforeEach(() => {
-    cy.visit('/nb/subjects/subject:8/topic:1:182562');
+    cy.server();
+    const visitOptions = {
+      onBeforeLoad: win => {
+        win.fetch = null;
+      },
+    };
+    cy.visit('/', visitOptions);
   });
   it('contains everything', () => {
-    cy.get('article');
-    cy.get('section:first-child h1').contains(/\w+/);
+    cy.get('[data-cy=subject-list] li:first-child a').click();
+    cy.get('[data-cy="topic-list"] li:first-child a').click();
+    cy.get('article > section:first-child').within(() => {
+      cy.get('h1').contains(/\w+/);
+      cy
+        .get('button')
+        .first()
+        .click();
+      cy.get('div[role="dialog"]').should('have.attr', 'aria-hidden', 'false');
+    });
+  });
+  it('send the needed server calls', () => {
+    cy.route('**/article-converter/**').as('articleApi');
+    cy.route('**/taxonomy/v1/topics/**').as('resourceCall');
+    cy
+      .route('**/taxonomy/v1/resource-types/?language=nb')
+      .as('resourceTypesCall');
+    cy.get('[data-cy=subject-list] li:first-child a').click();
+    cy.get('[data-cy="topic-list"] li:first-child a').click();
+    cy
+      .wait(['@articleApi', '@resourceCall', '@resourceTypesCall'])
+      .spread((article, resource, resourceTypes) => {
+        expect(article.response.body).to.be.an('object');
+        expect(resource.response.body).to.be.an('array');
+        expect(resourceTypes.response.body).to.be.an('array');
+      });
   });
 });
