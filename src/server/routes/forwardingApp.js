d91504f982d20b558c2cfab84d7b825d1caa546d
Henrik Nårstad <henrik.narstad@knowit.no>
2018-07-03 14:01:46 +0200

    keep language prefix in redirect url to new ndla #1144

--- src/server/routes/forwardingApp.js
+++ src/server/routes/forwardingApp.js
@@ -14,8 +14,14 @@ export async function forwardingApp(req, resp, next) {
   const token = await getToken();
   storeAccessToken(token.access_token);
 
+  const pathParts = req.originalUrl.substring(1).split('/');
+  const languagePrefix =
+    pathParts.length > 0 && pathParts[0] !== 'node' ? `/${pathParts[0]}` : '';
+
   const requestUrl = req.hostname + req.originalUrl;
   await taxonomyLookup(requestUrl)
-    .then(newPath => resp.redirect(301, `/subjects${newPath.path}`))
+    .then(newPath =>
+      resp.redirect(301, `${languagePrefix}/subjects${newPath.path}`),
+    )
     .catch(() => next());
 }
