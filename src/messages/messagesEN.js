3ccebb2395709455cb5e883679f942452e9a356a
Øyvind Marthinsen <oma@knowit.no>
2018-08-29 09:40:30 +0200

    refact:use messages from ndla-ui

---
+++ src/messages/messagesEN.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2018-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+const titleTemplate = ' - NDLA';
+
+const messages = {
+  htmlTitles: {
+    titleTemplate,
+    welcomePage: `Frontpage${titleTemplate}`,
+    topicPage: 'Topic',
+    subjectsPage: `Choose subjects${titleTemplate}`,
+    searchPage: `Search${titleTemplate}`,
+    notFound: `Page not found${titleTemplate}`,
+  },
+};
+
+export default messages;
---
+++ src/messages/messagesNN.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2018-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+const titleTemplate = ' - NDLA';
+
+const messages = {
+  htmlTitles: {
+    titleTemplate,
+    welcomePage: `Framsida${titleTemplate}`,
+    topicPage: 'Emne',
+    subjectsPage: `Velg fag${titleTemplate}`,
+    searchPage: `Søk${titleTemplate}`,
+    notFound: `Sida finst ikkje${titleTemplate}`,
+  },
+};
+
+export default messages;
