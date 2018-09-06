d68646135fec1c52f2b8f88c8ecb37a56342a4aa
Øyvind Marthinsen <oma@knowit.no>
2018-09-06 14:32:53 +0200

    fix: basename issue in RedirectExternal

--- src/components/RedirectExternal.js
+++ src/components/RedirectExternal.js
@@ -6,31 +6,52 @@
  *
  */
 
-import React, { Component } from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
-import Redirect from 'react-router-dom/Redirect';
 
 /**
  * The react-router Redirect component does'nt work with external
- * urls client side. So we use this helper component to handle it
- * for us.
+ * urls. So we use this helper component to handle it for us.
  */
 class RedirectExternal extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    const { staticContext } = this.context.router;
+
+    if (this.isStatic()) {
+      // Update static context serverside (see https://github.com/NDLANO/ndla-frontend/blob/master/src/server/helpers/render.js#L49)
+      staticContext.action = 'REPLACE';
+      staticContext.location = props.to;
+      staticContext.url = props.to;
+    }
+  }
+
   componentDidMount() {
-    window.location.replace(this.props.to);
+    // Probably clientside. Just use window.location.replace
+    if (!this.isStatic()) {
+      window.location.replace(this.props.to);
+    }
+  }
+
+  // Checks if we are using StaticRouter (i.e. serverside)
+  isStatic() {
+    return this.context.router && this.context.router.staticContext;
   }
 
   render() {
-    // Redirect component works with external urls serverside
-    if (process.env.BUILD_TARGET === 'SERVER') {
-      return <Redirect to={this.props.to} />;
-    }
     return null;
   }
 }
 
 RedirectExternal.propTypes = {
   to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
+};
+
+RedirectExternal.contextTypes = {
+  router: PropTypes.shape({
+    staticContext: PropTypes.object,
+  }).isRequired,
 };
 
 export default RedirectExternal;
