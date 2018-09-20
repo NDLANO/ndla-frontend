b8731e954230e5a2974909dae8a92555e990fa8b
Sebastian Jara <sebastian.jara@knowit.no>
2018-09-20 12:40:05 +0200

    refact: moved reducer to function, explanation

--- src/containers/WelcomePage/FrontpageSubjects.jsx
+++ src/containers/WelcomePage/FrontpageSubjects.jsx
@@ -1,4 +1,5 @@
 /**
+/**
  * Copyright (c) 2016-present, NDLA.
  *
  * This source code is licensed under the GPLv3 license found in the
@@ -41,49 +42,58 @@ export const getCategoriesWithAllSubjects = (
 ) => {
   // en is only valid for english nodes in old system
   const lang = locale === 'en' ? 'nb' : locale;
-  return categoriesFromApi.map(category => {
-    const newSubjects = category.subjects.map(categorySubject => ({
-      ...categorySubject,
-      text: categorySubject.name,
-      url: toSubject(categorySubject.id),
-      yearInfo: categorySubject.yearInfo,
-    }));
-    const oldSubjects = OLD_CATEGORIES_WITH_SUBJECTS[category.name]
-      .map(subject => ({
-        ...subject,
-        id: subject.nodeId,
-        text: subject.name,
-        url: subject.lang
-          ? `/${subject.lang}/node/${subject.nodeId}`
-          : `/${lang}/node/${subject.nodeId}`,
-        yearInfo: subject.yearInfo,
-      }))
-      .filter(
-        oldSubject =>
-          newSubjects.find(newSubject =>
-            oldSubject.name.startsWith(newSubject.name),
-          ) === undefined,
-      );
-    return {
-      [category.name]: {
-        ...category,
-        subjects: sortByName([...oldSubjects, ...newSubjects]),
-      },
-    };
-  });
+  return (
+    categoriesFromApi
+      .map(category => {
+        const newSubjects = category.subjects.map(categorySubject => ({
+          ...categorySubject,
+          text: categorySubject.name,
+          url: toSubject(categorySubject.id),
+          yearInfo: categorySubject.yearInfo,
+        }));
+        const oldSubjects = OLD_CATEGORIES_WITH_SUBJECTS[category.name]
+          .map(subject => ({
+            ...subject,
+            id: subject.nodeId,
+            text: subject.name,
+            url: subject.lang
+              ? `/${subject.lang}/node/${subject.nodeId}`
+              : `/${lang}/node/${subject.nodeId}`,
+            yearInfo: subject.yearInfo,
+          }))
+          .filter(
+            oldSubject =>
+              newSubjects.find(newSubject =>
+                oldSubject.name.startsWith(newSubject.name),
+              ) === undefined,
+          );
+        return {
+          [category.name]: {
+            ...category,
+            subjects: sortByName([...oldSubjects, ...newSubjects]),
+          },
+        };
+      })
+      /*
+  * NOTE: This reducer is needed in order to fit with the updated object
+  * structure in ndla-ui.
+  *
+  * Transform an array of categories to an object with list of categories.
+  *
+  * */
+      .reduce(
+        (obj, category) => ({
+          ...obj,
+          [category[Object.keys(category)].name]:
+            category[Object.keys(category)],
+        }),
+        {},
+      )
+  );
 };
 
 const FrontpageSubjects = ({ categories, subjects, locale }) => {
-  const frontpageCategories = getCategoriesWithAllSubjects(
-    categories,
-    locale,
-  ).reduce(
-    (obj, category) => ({
-      ...obj,
-      [category[Object.keys(category)].name]: category[Object.keys(category)],
-    }),
-    {},
-  );
+  const frontpageCategories = getCategoriesWithAllSubjects(categories, locale);
 
   const allSubjects = config.isNdlaProdEnvironment
     ? { ...frontpageCategories }
