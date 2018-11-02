NDLANO/ndla-frontend

commit be05d2d70ce8a346005d667c8a520c4b74e27c75

Fixes meta text problem missing because of to many articleIds in graphql #1375

src/queries.js
@@ -162,7 +162,8 @@ export const subjectTopicsQuery = gql`
       name
       path
       topics(all: true, filterIds: $filterIds) {
-        ...TopicInfo
+        id
+        name
       }
       filters {
         id
@@ -170,7 +171,6 @@ export const subjectTopicsQuery = gql`
       }
     }
   }
-  ${topicInfoFragment}
 `;
 
 export const subjectPageQuery = gql`
@@ -179,7 +179,7 @@ export const subjectPageQuery = gql`
       id
       name
       path
-      topics(all: true, filterIds: $filterIds) {
+      topics(filterIds: $filterIds) {
         ...TopicInfo
       }
       filters {

commit 54c811434b8ded1bfee38af5a8d4cd9cac5d2e87

fix: regression in be05d2d

src/queries.js
@@ -164,6 +164,8 @@ export const subjectTopicsQuery = gql`
       topics(all: true, filterIds: $filterIds) {
         id
         name
+        parent
+        path
       }
       filters {
         id