5f83f1d349ab32d708e492e95c7727f992605a76
Christian Peter <christian150790@gmail.com>
2018-12-12 15:43:00 +0100

    Revert prettier changes

--- razzle.config.js
+++ razzle.config.js
@@ -1,55 +1,55 @@
-const { modifyRule } = require("razzle-config-utils");
-const webpack = require("webpack"); // eslint-disable-line import/no-extraneous-dependencies
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const { modifyRule } = require('razzle-config-utils');
+const webpack = require('webpack'); // eslint-disable-line import/no-extraneous-dependencies
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   modify(config, { target, dev }) {
     const appConfig = config;
 
     modifyRule(appConfig, { test: /\.css$/ }, rule => {
-      rule.use.push({ loader: "postcss-loader" });
-      rule.use.push({ loader: "sass-loader" });
+      rule.use.push({ loader: 'postcss-loader' });
+      rule.use.push({ loader: 'sass-loader' });
     });
 
     appConfig.module.rules.shift(); // remove eslint-loader
-    if (target === "web") {
+    if (target === 'web') {
       appConfig.output.filename = dev
-        ? "static/js/[name].js"
-        : "static/js/[name].[hash:8].js";
+        ? 'static/js/[name].js'
+        : 'static/js/[name].[hash:8].js';
 
       if (dev) {
         appConfig.entry.embed = [
           appConfig.entry.client[0],
           appConfig.entry.client[1],
-          "./src/iframe"
+          './src/iframe',
         ];
         appConfig.entry.ltiEmbed = [
           appConfig.entry.client[0],
           appConfig.entry.client[1],
-          "./src/lti"
+          './src/lti',
         ];
-        appConfig.entry.injectCss = ["./src/style/index.css"];
+        appConfig.entry.injectCss = ['./src/style/index.css'];
       } else {
-        appConfig.entry.embed = ["./src/iframe"];
-        appConfig.entry.ltiEmbed = ["./src/lti"];
+        appConfig.entry.embed = ['./src/iframe'];
+        appConfig.entry.ltiEmbed = ['./src/lti'];
       }
 
       appConfig.entry.mathJaxConfig = dev
-        ? [appConfig.entry.client[1], "./src/mathjax/config.js"]
-        : ["./src/mathjax/config.js"];
+        ? [appConfig.entry.client[1], './src/mathjax/config.js']
+        : ['./src/mathjax/config.js'];
 
       if (!dev) {
         appConfig.plugins.push(
           new BundleAnalyzerPlugin({
-            analyzerMode: "static",
+            analyzerMode: 'static',
             openAnalyzer: false,
-            reportFilename: "bundle-analyzer-report.html"
+            reportFilename: 'bundle-analyzer-report.html',
           }),
-          new webpack.optimize.ModuleConcatenationPlugin()
+          new webpack.optimize.ModuleConcatenationPlugin(),
         );
       }
       appConfig.performance = {
-        hints: false
+        hints: false,
       };
     }
 
@@ -63,11 +63,11 @@ module.exports = {
     }
 
     if (!dev) {
-      appConfig.devtool = "source-map";
+      appConfig.devtool = 'source-map';
     } else {
-      appConfig.devtool = "eval-source-map";
+      appConfig.devtool = 'eval-source-map';
     }
 
     return appConfig;
-  }
+  },
 };
