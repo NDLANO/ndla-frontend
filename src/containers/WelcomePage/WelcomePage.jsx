7dca9839ee6143e0cd498feec304e312484bea55
Tale Prestmo <tale.prestmo@knowit.no>
2019-04-23 09:42:47 +0200

    Fix url for frontpage film

--- src/containers/WelcomePage/WelcomePage.jsx
+++ src/containers/WelcomePage/WelcomePage.jsx
@@ -24,6 +24,7 @@ import { runQueries } from '../../util/runQueries';
 import WelcomePageInfo from './WelcomePageInfo';
 import { DefaultErrorMessage } from '../../components/DefaultErrorMessage';
 import FrontpageSubjects from './FrontpageSubjects';
+import { FILM_PAGE_PATH } from '../../constants';
 
 export class WelcomePage extends Component {
   constructor() {
@@ -124,7 +125,7 @@ export class WelcomePage extends Component {
             />
             <FrontpageFilm
               imageUrl="/static/film_illustrasjon.svg"
-              url="/film"
+              url={FILM_PAGE_PATH}
               messages={{
                 header: t('welcomePage.film.header'),
                 linkLabel: t('welcomePage.film.linkLabel'),
