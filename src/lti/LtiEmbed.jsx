cae7ca7e6b1a2f45186eb2087bd3f343cae623b1
Christian Peter <chrpeter@Christians-MacBook-Pro.local>
2019-07-09 13:52:09 +0200

    Fixes license text

--- src/lti/LtiEmbed.jsx
+++ src/lti/LtiEmbed.jsx
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019-present, NDLA.
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import { LtiDataShape } from '../shapes';
