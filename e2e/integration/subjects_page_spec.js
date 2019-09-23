c204325a631b1475c8e6ab1efafbd844d939ebc5
Gunnar Velle <gv@knowit.no>
2019-09-23 09:24:17 +0200

    Fix tests after missing class

--- e2e/integration/subjects_page_spec.js
+++ e2e/integration/subjects_page_spec.js
@@ -38,7 +38,7 @@ describe('Subjects page', () => {
   });
 
   it('should have a valid breadcrumb, filter and language select', () => {
-    cy.get('.c-breadcrumb a')
+    cy.get('.c-breadcrumb__list a')
       .should('have.length', 1)
       .and('have.attr', 'href');
 
