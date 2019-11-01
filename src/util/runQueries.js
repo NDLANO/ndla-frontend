0cbf34cac226cc5ad1a73819a2967f09aa6aed21
Torgeir Haugen <torhau@knowit.no>
2019-11-01 13:18:49 +0100

    Remove last of runQueries

--- src/util/runQueries.js
+++ src/util/runQueries.js
@@ -8,38 +8,6 @@ import { useQuery } from 'react-apollo';
  *
  */
 
-const mergeError = (obj, result) => {
-  if (!result.errors) {
-    return [...obj.errors];
-  }
-  return [...obj.errors, ...result.errors];
-};
-
-export async function runQueries(client, queries) {
-  const results = await Promise.all(
-    queries.map(async options =>
-      client.query({
-        errorPolicy: 'all',
-        ...options,
-      }),
-    ),
-  );
-
-  const mergedResults = results.reduce(
-    (obj, result) => ({
-      errors: mergeError(obj, result),
-      data: { ...obj.data, ...result.data },
-    }),
-    { errors: [] },
-  );
-
-  if (mergedResults.errors.length === 0) {
-    delete mergedResults.errors;
-  }
-
-  return mergedResults;
-}
-
 export const useGraphQuery = (query, options = {}) => {
   const { error, data, loading } = useQuery(query, {
     errorPolicy: 'all',
