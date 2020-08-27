44cd358f08faa41cdc992ec480708e2350d6c9b8
Gunnar Velle <gunnar.velle@gmail.com>
2020-08-27 11:48:19 +0200

    Hide subject info

--- src/containers/SubjectPage/SubjectContainer.jsx
+++ src/containers/SubjectPage/SubjectContainer.jsx
@@ -272,12 +272,12 @@ const SubjectPage = ({
           negativeTopMargin={moveBannerUp}
         />
       )}
-      {subjectpage.about && (
+      {false && subjectpage.about && (
         <OneColumn wide>
           <SubjectPageInformation subjectpage={subjectpage} wide />
         </OneColumn>
       )}
-      {editorsChoices && (
+      {false && editorsChoices && (
         <SubjectEditorChoices
           wideScreen
           editorsChoices={editorsChoices}
