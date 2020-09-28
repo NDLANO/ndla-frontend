6455ab8f45876dcdc4a247b00da6e281ef209b98
beery <me@bjoernar.com>
2020-09-28 10:56:17 +0200

    Remove "Yrkesfaglig fordypning" from "Bygg og anlegg"

--- src/data/programmes.js
+++ src/data/programmes.js
@@ -32,9 +32,6 @@ export const programmes = [
               {
                 id: 'programme_subject_43',
               },
-              {
-                id: 'programme_subject_45',
-              },
             ],
           },
           {
