5c2bba82a151da58d94d2f69d96505c9462a5531
Gunnar Velle <gunnar.velle@gmail.com>
2021-04-20 08:14:25 +0200

    Remove custom articletype used for ndla-film in test.

--- src/containers/FilmFrontpage/NdlaFilmFrontpage.jsx
+++ src/containers/FilmFrontpage/NdlaFilmFrontpage.jsx
@@ -19,7 +19,6 @@ import {
 } from '../../queries';
 import { movieResourceTypes } from './resourceTypes';
 import MoreAboutNdlaFilm from './MoreAboutNdlaFilm';
-import config from '../../config';
 
 const ALL_MOVIES_ID = 'ALL_MOVIES_ID';
 
@@ -35,8 +34,6 @@ const NdlaFilm = ({ t, locale, skipToContentId }) => {
     variables: { subjectId: 'urn:subject:20', filterIds: '' },
   });
   const [searchAllMovies, { data: allMovies }] = useLazyQuery(searchFilmQuery);
-  // TODO: When ff is phased out, always use standard.
-  const contextType = config.ndlaFilmArticleType;
 
   useEffect(() => {
     // if we receive new movies we map them into state
@@ -67,7 +64,7 @@ const NdlaFilm = ({ t, locale, skipToContentId }) => {
       variables: {
         subjects: 'urn:subject:20',
         resourceTypes,
-        contextTypes: contextType,
+        contextTypes: 'standard',
       },
     });
   };
