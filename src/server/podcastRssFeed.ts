38c83cb1efaced92ba0d6baff6347a3b5cdf52b0
Jonas Natten <jonas.natten@knowit.no>
2021-06-15 10:28:37 +0200

    Copyright headers

--- src/server/podcastRssFeed.ts
+++ src/server/podcastRssFeed.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2021-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import config from 'config';
 import { fetchSeries } from '../util/audioApi';
 
