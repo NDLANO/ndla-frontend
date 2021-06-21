a166e76a48a3843394a29912afcb660b4c195da1
Gunnar Velle <gunnar.velle@gmail.com>
2021-06-21 07:09:58 +0200

    Formatting topicsHelper.js

--- src/util/topicsHelper.js
+++ src/util/topicsHelper.js
@@ -11,7 +11,10 @@ import groupBy from './groupBy';
 import { fixEndSlash } from '../routeHelpers';
 
 export const groupedSubtopicsByParent = (topics = []) =>
-  groupBy(topics.filter(topic => topic.parent), 'parent');
+  groupBy(
+    topics.filter(topic => topic.parent),
+    'parent',
+  );
 
 export const toTopicMenu = (topic, topics) => {
   const groupedSubTopics = groupedSubtopicsByParent(topics);
