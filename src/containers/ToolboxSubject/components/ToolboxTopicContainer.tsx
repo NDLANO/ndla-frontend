6f4ac8246dc1ffc8ffce613df69f9a19b3a11d8a
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2021-09-21 16:12:42 +0200

    Add header to ToolboxTopicContainer

--- src/containers/ToolboxSubject/components/ToolboxTopicContainer.tsx
+++ src/containers/ToolboxSubject/components/ToolboxTopicContainer.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2021-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import React from 'react';
 //@ts-ignore
 import { Spinner } from '@ndla/ui';
