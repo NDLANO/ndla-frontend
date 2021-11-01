f851ad84163c8ff31d6fcff4ab386ffac6f7bf43
aod <anders.dahlin@knowit.no>
2021-11-01 06:36:33 +0100

    copyright

--- src/components/FeideLoginButton/FeideLoginButton.tsx
+++ src/components/FeideLoginButton/FeideLoginButton.tsx
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import React, { ReactElement, useContext } from 'react';
 import { RouteProps } from 'react-router';
 
