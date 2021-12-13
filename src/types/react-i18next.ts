7a40587a275b75b8596bb772c2d9ea7b048d82f3
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2021-12-13 14:30:53 +0100

    Converted Footer to TypeScript. Chnged getLocaleUrls return type to satisfy TypeScript. Removed location drilling from Page

---
+++ src/types/react-i18next.ts
@@ -0,0 +1,46 @@
+import { Callback, i18n } from 'i18next';
+import { ComponentProps, ComponentType } from 'react';
+import { LocaleType } from '../interfaces';
+
+declare module 'react-i18next' {
+  interface CustomI18n extends Omit<i18n, 'language' | 'changeLanguage'> {
+    language: LocaleType;
+    changeLanguage: (
+      lng: LocaleType,
+      callback?: Callback,
+    ) => Promise<TFunction>;
+  }
+
+  interface CustomUseTranslationResponse<
+    N extends Namespace = DefaultNamespace
+  > extends Omit<UseTranslationResponse<N>, 'i18n'> {
+    i18n: CustomI18n;
+  }
+  interface CustomWithTranslation<N extends Namespace = DefaultNamespace> {
+    t: TFunction<N>;
+    i18n: CustomI18n;
+    tReady: boolean;
+  }
+
+  function useTranslation<N extends Namespace = DefaultNamespace>(
+    ns?: N | Readonly<N>,
+    options?: UseTranslationOptions,
+  ): CustomUseTranslationResponse<N>;
+
+  function withTranslation<N extends Namespace = DefaultNamespace>(
+    ns?: N,
+    options?: {
+      withRef?: boolean;
+    },
+  ): <
+    C extends ComponentType<ComponentProps<C> & WithTranslationProps>,
+    ResolvedProps = JSX.LibraryManagedAttributes<
+      C,
+      Subtract<ComponentProps<C>, WithTranslationProps>
+    >
+  >(
+    component: C,
+  ) => ComponentType<
+    Omit<ResolvedProps, keyof CustomWithTranslation<N>> & WithTranslationProps
+  >;
+}
