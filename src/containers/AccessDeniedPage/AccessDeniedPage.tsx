5d454862b8a728735082cd2e0a0c47e513c39bb4
Jonas Natten <jonas.natten@knowit.no>
2021-12-15 13:12:13 +0100

    Redirect to login when clicking login button on `AccessDeniedPage`

--- src/containers/AccessDeniedPage/AccessDeniedPage.tsx
+++ src/containers/AccessDeniedPage/AccessDeniedPage.tsx
@@ -10,11 +10,14 @@ import React, { useContext } from 'react';
 import { OneColumn, ErrorResourceAccessDenied } from '@ndla/ui';
 import { HelmetWithTracker } from '@ndla/tracker';
 import { useTranslation } from 'react-i18next';
+import { useHistory, useLocation } from 'react-router';
 import { Status } from '../../components';
 import { AuthContext } from '../../components/AuthenticationContext';
 
 const AccessDenied = () => {
   const { t } = useTranslation();
+  const location = useLocation();
+  const history = useHistory();
   const { authenticated } = useContext(AuthContext);
   const statusCode = authenticated ? 403 : 401;
 
@@ -22,7 +25,16 @@ const AccessDenied = () => {
     <Status code={statusCode}>
       <HelmetWithTracker title={t('htmlTitles.accessDenied')} />
       <OneColumn cssModifier="clear">
-        <ErrorResourceAccessDenied onAuthenticateClick={() => {}} />
+        <ErrorResourceAccessDenied
+          onAuthenticateClick={() => {
+            location && localStorage.setItem('lastPath', location.pathname);
+            if (authenticated) {
+              history.push('/logout');
+            } else {
+              history.push('/login');
+            }
+          }}
+        />
       </OneColumn>
     </Status>
   );
