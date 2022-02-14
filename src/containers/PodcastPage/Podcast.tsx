453a53a356d73e08a38114acc259cb98913b6750
Henrik Liodden <henrik.liodden@knowit.no>
2022-02-14 09:49:20 +0100

    Dont display podcast license modal on page opening

--- src/containers/PodcastPage/Podcast.tsx
+++ src/containers/PodcastPage/Podcast.tsx
@@ -26,6 +26,7 @@ import CopyTextButton from '../../components/license/CopyTextButton';
 import AnchorButton from '../../components/license/AnchorButton';
 import config from '../../config';
 import { GQLAudio } from '../../graphqlTypes';
+import { uuid } from '@ndla/util';
 
 interface Props {
   podcast: GQLAudio;
@@ -72,58 +73,63 @@ const Podcast = ({ podcast }: Props) => {
     type: item.label,
   }));
 
+  const id = uuid();
+  const figureId = `figure-${id}`;
+
   return (
-    <Figure id={`figure-${podcast.id}`} type="full-column">
-      <AudioPlayer
-        src={podcast.audioFile.url}
-        title={podcast.title.title}
-        description={podcast.podcastMeta?.introduction}
-        img={coverPhoto}
-        textVersion={podcast.manuscript?.manuscript}
-      />
-      <FigureCaption
-        figureId={`figure-${podcast.id}`}
-        id={`${podcast.id}`}
-        locale={language}
-        key="caption"
-        caption={podcast.title.title}
-        licenseRights={license?.rights || []}
-        reuseLabel={t('other.reuse')}
-        authors={contributors}>
-        <FigureLicenseDialog
-          id={`${podcast.id}`}
-          authors={contributors}
-          locale={language}
-          license={getLicenseByAbbreviation(
-            podcast.copyright.license?.license!,
-            'nb',
-          )}
-          messages={messages}
+    <div>
+      <Figure id={figureId} type="full-column">
+        <AudioPlayer
+          src={podcast.audioFile.url}
           title={podcast.title.title}
-          origin={podcast.copyright.origin}>
-          <CopyTextButton
-            stringToCopy={getCopyString(
-              podcast.title.title,
-              undefined,
-              `/podcast/${podcast.id}`,
-              podcast.copyright,
-              config.ndlaFrontendDomain,
-              key => t(key),
+          description={podcast.podcastMeta?.introduction}
+          img={coverPhoto}
+          textVersion={podcast.manuscript?.manuscript}
+        />
+        <FigureCaption
+          figureId={figureId}
+          id={id}
+          locale={language}
+          key="caption"
+          caption={podcast.title.title}
+          licenseRights={license?.rights || []}
+          reuseLabel={t('other.reuse')}
+          authors={contributors}>
+          <FigureLicenseDialog
+            id={id}
+            authors={contributors}
+            locale={language}
+            license={getLicenseByAbbreviation(
+              podcast.copyright.license?.license!,
+              'nb',
             )}
-            copyTitle={t('license.copyTitle')}
-            hasCopiedTitle={t('license.hasCopiedTitle')}
-          />
-          {podcast.copyright.license?.license !== 'COPYRIGHTED' && (
-            <AnchorButton
-              href={podcast.audioFile.url}
-              download
-              appearance="outline">
-              {t('license.download')}
-            </AnchorButton>
-          )}
-        </FigureLicenseDialog>
-      </FigureCaption>
-    </Figure>
+            messages={messages}
+            title={podcast.title.title}
+            origin={podcast.copyright.origin}>
+            <CopyTextButton
+              stringToCopy={getCopyString(
+                podcast.title.title,
+                undefined,
+                `/podcast/${podcast.id}`,
+                podcast.copyright,
+                config.ndlaFrontendDomain,
+                key => t(key),
+              )}
+              copyTitle={t('license.copyTitle')}
+              hasCopiedTitle={t('license.hasCopiedTitle')}
+            />
+            {podcast.copyright.license?.license !== 'COPYRIGHTED' && (
+              <AnchorButton
+                href={podcast.audioFile.url}
+                download
+                appearance="outline">
+                {t('license.download')}
+              </AnchorButton>
+            )}
+          </FigureLicenseDialog>
+        </FigureCaption>
+      </Figure>
+    </div>
   );
 };
 
