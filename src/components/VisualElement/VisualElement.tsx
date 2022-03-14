829b08826873e2bcd56751103137f1ae1c87ec3b
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-03-14 14:50:39 +0100

    Add missing copyright

--- src/components/VisualElement/VisualElement.tsx
+++ src/components/VisualElement/VisualElement.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2021 -present, NDLA
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { gql } from '@apollo/client';
 import { Image } from '@ndla/ui';
 import { GQLVisualElement_VisualElementFragment } from '../../graphqlTypes';
