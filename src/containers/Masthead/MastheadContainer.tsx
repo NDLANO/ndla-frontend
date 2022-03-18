a128deddf68e5b5862e17c86e738731a03c032d5
Henrik Liodden <henrik.liodden@knowit.no>
2022-03-18 14:31:57 +0100

    Add alerts to Masthead

--- src/containers/Masthead/MastheadContainer.tsx
+++ src/containers/Masthead/MastheadContainer.tsx
@@ -16,7 +16,7 @@ import {
   BreadcrumbBlock,
 } from '@ndla/ui';
 import { RouteComponentProps } from 'react-router';
-import { useLazyQuery } from '@apollo/client';
+import { useLazyQuery, useQuery } from '@apollo/client';
 
 import { Feide } from '@ndla/icons/common';
 import { useTranslation } from 'react-i18next';
@@ -25,7 +25,7 @@ import { getUrnIdsFromProps, toBreadcrumbItems } from '../../routeHelpers';
 import FeideLoginButton from '../../components/FeideLoginButton';
 import MastheadSearch from './components/MastheadSearch';
 import MastheadMenu from './components/MastheadMenu';
-import { mastHeadQuery } from '../../queries';
+import { alertsQuery, mastHeadQuery } from '../../queries';
 import { getLocaleUrls } from '../../util/localeHelpers';
 import ErrorBoundary from '../ErrorPage/ErrorBoundary';
 import { mapMastheadData } from './mastheadHelpers';
@@ -37,6 +37,7 @@ import { getProgrammeBySlug } from '../../data/programmes';
 import { mapGradesData } from '../ProgrammePage/ProgrammePage';
 import { LocaleType } from '../../interfaces';
 import {
+  GQLAlertsQuery,
   GQLMastHeadQuery,
   GQLMastHeadQueryVariables,
   GQLResourceType,
@@ -74,6 +75,8 @@ const MastheadContainer = ({
   const [topicId, setTopicId] = useState('');
   const [state, setState] = useState<State>({});
   const { t, i18n } = useTranslation();
+
+  const { data: alertData } = useQuery<GQLAlertsQuery>(alertsQuery);
 
   useEffect(() => {
     updateData();
@@ -162,13 +165,16 @@ const MastheadContainer = ({
       />
     );
 
+  const alerts = alertData?.alerts?.map(alert => alert.body || alert.title);
+
   return (
     <ErrorBoundary>
       <Masthead
         fixed
         ndlaFilm={ndlaFilm}
         skipToMainContentId={skipToMainContentId}
-        infoContent={infoContent}>
+        infoContent={infoContent}
+        messages={alerts}>
         <MastheadItem left>
           <MastheadMenu
             subject={subject}
@@ -191,10 +197,8 @@ const MastheadContainer = ({
                         .slice(1)
                         .map(uri => ({ name: uri.name!, to: uri.to! }))
                     : []
-                }>
-                {/* Requires a child */}
-                <></>
-              </BreadcrumbBlock>
+                }
+              />
             </DisplayOnPageYOffset>
           )}
         </MastheadItem>
