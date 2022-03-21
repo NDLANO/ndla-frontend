2b296c302fce6fd1aab1b4214b01306ff1ce2174
Henrik Liodden <henrik.liodden@knowit.no>
2022-03-21 10:19:36 +0100

    Migrate from deprecated eslint graphql library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,22 @@
-const { specifiedRules: allGraphqlRules } = require('graphql');
-
-// issue: https://github.com/apollographql/eslint-plugin-graphql/issues/19
-// After updating graphql to 15, eslint is complaining about string literals in src/queries.js file.
-// We are bypassing the problem by excluding some eslint rules for graphql string literals.
-const validators = allGraphqlRules
-  .map(rule => rule.name)
-  .filter(ruleName => [
-    'NoUnusedFragments',
-    'KnownFragmentNames',
-    'NoUnusedVariables'
-  ].includes(ruleName));
-
 module.exports = {
-  "extends": "eslint-config-ndla",
-  "rules": {
-    "graphql/template-strings": [ "error", {
-        "env": "literal",
-        "schemaJson": require("./src/gqlSchema.json"),
-        validators
-      }, {
-        "env": "apollo",
-        "schemaJson": require("./src/gqlSchema.json"),
-      }],
-    "react/prop-types": [2, { "ignore": ["children", "className", "t"] }]
-  },
-  "plugins": ["graphql"]
-}
+  extends: 'eslint-config-ndla',
+  overrides: [
+    {
+      files: ['*.{js,jsx,ts,tsx}'],
+      processor: '@graphql-eslint/graphql',
+    },
+    {
+      files: ['*.graphql'],
+      parser: '@graphql-eslint/eslint-plugin',
+      plugins: ['@graphql-eslint'],
+      rules: {
+        '@graphql-eslint/known-type-names': 'error',
+      },
+      parserOptions: {
+        schema: './src/schema.graphql',
+      },
+    },
+  ],
+  plugins: ['@graphql-eslint'],
+  ignorePatterns: ['./src/schema.graphql'],
+};
