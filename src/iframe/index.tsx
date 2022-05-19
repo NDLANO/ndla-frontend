e54e964482e2961b74434665adf32df48ade54fd
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-05-19 08:25:08 +0200

    Add i18nextprovider to iframe client

--- src/iframe/index.tsx
+++ src/iframe/index.tsx
@@ -7,8 +7,10 @@
  */
 
 import ReactDOM from 'react-dom';
+import { I18nextProvider } from 'react-i18next';
 import { HelmetProvider } from 'react-helmet-async';
 import { ApolloProvider } from '@apollo/client';
+import { i18nInstance } from '@ndla/ui';
 import { configureTracker } from '@ndla/tracker';
 import ErrorReporter from '@ndla/error-reporter';
 import { CacheProvider } from '@emotion/core';
@@ -19,6 +21,7 @@ import IframePageContainer from './IframePageContainer';
 import { createHistory } from '../history';
 import { EmotionCacheKey } from '../constants';
 import { createApolloClient } from '../util/apiHelpers';
+import { initializeI18n } from '../i18n';
 
 const { config, initialProps } = window.DATA;
 
@@ -48,19 +51,26 @@ const cache = createCache({ key: EmotionCacheKey });
 
 const client = createApolloClient(initialProps.locale, initialProps.resCookie);
 
+const i18n = initializeI18n(
+  i18nInstance,
+  initialProps.locale ?? config.defaultLocale,
+);
+
 const renderOrHydrate = disableSSR ? ReactDOM.render : ReactDOM.hydrate;
 renderOrHydrate(
-  <HelmetProvider>
-    <ApolloProvider client={client}>
-      <CacheProvider value={cache}>
-        <Router history={browserHistory}>
-          <CompatRouter>
-            <IframePageContainer {...initialProps} />
-          </CompatRouter>
-        </Router>
-      </CacheProvider>
-    </ApolloProvider>
-  </HelmetProvider>,
+  <I18nextProvider i18n={i18n}>
+    <HelmetProvider>
+      <ApolloProvider client={client}>
+        <CacheProvider value={cache}>
+          <Router history={browserHistory}>
+            <CompatRouter>
+              <IframePageContainer {...initialProps} />
+            </CompatRouter>
+          </Router>
+        </CacheProvider>
+      </ApolloProvider>
+    </HelmetProvider>
+  </I18nextProvider>,
   document.getElementById('root'),
   () => {
     window.hasHydrated = true;
