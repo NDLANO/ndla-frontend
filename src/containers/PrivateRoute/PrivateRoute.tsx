02e62f53a120009f6d2eecad2464ea43d194f9ef
Magnus L. Holtet <dev@holtet.me>
2022-06-10 13:43:57 +0200

    :green_heart: Trigger CI

--- src/containers/PrivateRoute/PrivateRoute.tsx
+++ src/containers/PrivateRoute/PrivateRoute.tsx
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-
 import { ReactElement, useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../components/AuthenticationContext';
