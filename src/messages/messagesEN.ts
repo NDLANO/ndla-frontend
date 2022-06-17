5146d25f584f1e60bb817241e82d4330a3fdac75
ghveem <guttorm@hveem.no>
2022-06-17 12:39:34 +0200

    Nye blogginnlegg på forsida.

--- src/messages/messagesEN.ts
+++ src/messages/messagesEN.ts
@@ -37,17 +37,17 @@ const messages = {
   blogPosts: {
     blog1: {
       imageUrl: '/static/nye-fag.jpg',
-      text: 'Nye fag på NDLA',
-      externalLink: 'https://blogg.ndla.no/2021/12/nye-fag-pa-ndla/',
+      text: 'Forslag til årsplaner fra NDLA',
+      externalLink: 'https://blogg.ndla.no/2021/08/forslag-til-arsplaner-hos-ndla/',
       linkText: 'Fagblogg',
       license: 'CC-BY-SA-4.0',
       licenseAuthor: 'Vibeke Klungland',
     },
     blog2: {
       imageUrl: '/static/aktiviser-elevene.jpg',
-      text: 'Aktiviser elevane med digitale verktøy',
+      text: 'Huskeliste for kontaktlærere',
       externalLink:
-        'https://blogg.ndla.no/2021/09/aktiviser-elevane-med-digitale-verktoy/',
+        'https://blogg.ndla.no/2019/08/huskeliste-for-kontaktlaerere/',
       linkText: 'Fagblogg',
       license: 'CC-BY-SA-4.0',
       licenseAuthor: 'Tom Knudsen',
--- src/messages/messagesNN.ts
+++ src/messages/messagesNN.ts
@@ -37,8 +37,8 @@ const messages = {
   blogPosts: {
     blog1: {
       imageUrl: '/static/nye-fag.jpg',
-      text: 'Nye fag på NDLA',
-      externalLink: 'https://blogg.ndla.no/2021/12/nye-fag-pa-ndla/',
+      text: 'Forslag til årsplaner fra NDLA',
+      externalLink: 'https://blogg.ndla.no/2021/08/forslag-til-arsplaner-hos-ndla/',
       linkText: 'Fagblogg',
       license: 'CC-BY-SA-4.0',
       licenseAuthor: 'Vibeke Klungland',
@@ -47,7 +47,7 @@ const messages = {
       imageUrl: '/static/aktiviser-elevene.jpg',
       text: 'Aktiviser elevene med digitale verktøy',
       externalLink:
-        'https://blogg.ndla.no/2021/09/aktiviser-elevane-med-digitale-verktoy/',
+        'https://blogg.ndla.no/2019/08/huskeliste-for-kontaktlaerere/',
       linkText: 'Fagblogg',
       license: 'CC-BY-SA-4.0',
       licenseAuthor: 'Tom Knudsen',
