faa222a1c40899a38c535812cc9872a4fb48eb06
Jonas Natten <jonas.natten@knowit.no>
2022-09-15 18:53:05 +0200

    Bump @apollo/client to latest version
    The change in razzle.config.js is to make webpack load ESM modules
    before commonjs on the serverside. Without this change the
    `@apollo/client` was not bundled in the server build.

--- razzle.config.js
+++ razzle.config.js
@@ -53,6 +53,11 @@ module.exports = {
       // It triggers a couple of «Critical dependency: the request of a dependency is an
       // expression warning» which we can safely ignore.
       appConfig.externals = [];
+
+      // This tells webpack to resolve esm modules before commonjs on the server-side.
+      // @apollo/client stopped being bundled without this when moving from 3.4.x to 3.5.x
+      appConfig.resolve.mainFields = ['module', 'main'];
+
       // Razzle/CRA breaks the build on webpack warnings. Disable CI env to circumvent the check.
       process.env.CI = 'false';
     }
