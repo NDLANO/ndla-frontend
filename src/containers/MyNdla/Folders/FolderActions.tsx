947fe597a1a0da313b3156049cf78da64a8466fe
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-09-22 13:34:38 +0200

    Add missing licenses

--- src/containers/MyNdla/Folders/FolderActions.tsx
+++ src/containers/MyNdla/Folders/FolderActions.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { MenuButton, MenuItemProps } from '@ndla/button';
 import { Pencil } from '@ndla/icons/lib/action';
 import { DeleteForever } from '@ndla/icons/lib/editor';
