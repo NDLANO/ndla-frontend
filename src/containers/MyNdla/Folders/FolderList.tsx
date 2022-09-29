ded7e53fb3de8d6f4487400f7ec49a752a23c5e3
Jonas Natten <jonas.natten@knowit.no>
2022-09-29 10:07:21 +0200

    Refactor Folder+Resource lists
    Split them up into multiple components to improve readability

--- src/containers/MyNdla/Folders/FolderList.tsx
+++ src/containers/MyNdla/Folders/FolderList.tsx
@@ -6,19 +6,15 @@
  *
  */
 
-import { Folder, MakeDNDList } from '@ndla/ui';
-import { Pencil } from '@ndla/icons/action';
-import { useTranslation } from 'react-i18next';
-import { DeleteForever } from '@ndla/icons/editor';
-import {
-  DraggableProvidedDragHandleProps,
-  DropResult,
-} from 'react-beautiful-dnd';
+import { MakeDNDList } from '@ndla/ui';
+import { DropResult } from 'react-beautiful-dnd';
 import { useApolloClient } from '@apollo/client';
 import { GQLFolder } from '../../../graphqlTypes';
 import { FolderTotalCount } from '../../../util/folderHelpers';
-import { FolderAction, ListItem, ViewType } from './FoldersPage';
+import { FolderAction, ViewType } from './FoldersPage';
 import { useSortFoldersMutation } from '../folderMutations';
+import DraggableFolder from './DraggableFolder';
+import { moveIndexToNewIndex } from './util';
 
 interface Props {
   currentFolderId: string | undefined;
@@ -27,72 +23,8 @@ interface Props {
   foldersCount: Record<string, FolderTotalCount>;
   setFolderAction: (action: FolderAction | undefined) => void;
 }
-interface DraggableFolderProps {
-  id: string;
-  type: ViewType;
-  folder: GQLFolder;
-  foldersCount: Record<string, FolderTotalCount>;
-  setFolderAction: (action: FolderAction | undefined) => void;
-  index: number;
-  dragHandleProps?: DraggableProvidedDragHandleProps;
-}
 
-const DraggableFolder = ({
-  index,
-  type,
-  foldersCount,
-  setFolderAction,
-  folder,
-  dragHandleProps,
-}: DraggableFolderProps) => {
-  const { t } = useTranslation();
-  return (
-    <ListItem
-      key={`folder-${folder.id}`}
-      id={`folder-${folder.id}`}
-      tabIndex={-1}
-      {...dragHandleProps}>
-      <Folder
-        key={folder.id}
-        id={folder.id}
-        link={`/minndla/folders/${folder.id}`}
-        title={folder.name}
-        type={type === 'block' ? 'block' : 'list'}
-        subFolders={foldersCount[folder.id]?.folders}
-        subResources={foldersCount[folder.id]?.resources}
-        menuItems={[
-          {
-            icon: <Pencil />,
-            text: t('myNdla.folder.edit'),
-            onClick: () => setFolderAction({ action: 'edit', folder, index }),
-          },
-          {
-            icon: <DeleteForever />,
-            text: t('myNdla.folder.delete'),
-            onClick: () => setFolderAction({ action: 'delete', folder, index }),
-            type: 'danger',
-          },
-        ]}
-      />
-    </ListItem>
-  );
-};
-
-const moveIndexToNewIndex = <T,>(
-  array: T[],
-  oldIdx: number,
-  newIdx: number,
-): T[] | null => {
-  const copy = [...array];
-  const toMove = copy[oldIdx];
-  if (!toMove) return null;
-
-  copy.splice(oldIdx, 1); // Remove moved item from list
-  copy.splice(newIdx, 0, toMove); // Insert removed item to new location
-  return copy;
-};
-
-const DraggableFolderList = ({
+const FolderList = ({
   currentFolderId,
   type,
   folders,
@@ -111,8 +43,12 @@ const DraggableFolderList = ({
     if (destinationIdx === undefined) return;
 
     const originalIds = folders.map(f => f.id);
-    const ids = moveIndexToNewIndex(originalIds, sourceIdx, destinationIdx);
-    if (ids === null) return;
+    const sortedIds = moveIndexToNewIndex(
+      originalIds,
+      sourceIdx,
+      destinationIdx,
+    );
+    if (sortedIds === null) return;
 
     const updateCache = (newOrder: string[]) => {
       const sortCacheModifierFunction = (
@@ -137,11 +73,12 @@ const DraggableFolderList = ({
       }
     };
 
-    updateCache(ids);
+    // Update cache before sorting happens to make gui feel snappy
+    updateCache(sortedIds);
 
     return sortFolders({
       variables: {
-        sortedIds: ids,
+        sortedIds,
         parentId,
       },
     }).catch(() => updateCache(originalIds));
@@ -168,4 +105,4 @@ const DraggableFolderList = ({
   );
 };
 
-export default DraggableFolderList;
+export default FolderList;
