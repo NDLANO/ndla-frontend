d7c4c7c731b821cbb8b449287075f22461316e4a
Jonas Natten <jonas.natten@knowit.no>
2022-10-04 08:17:46 +0200

    WIP

--- src/containers/MyNdla/Folders/DraggableFolder.tsx
+++ src/containers/MyNdla/Folders/DraggableFolder.tsx
@@ -9,11 +9,13 @@
 import { Folder } from '@ndla/ui';
 import { Pencil } from '@ndla/icons/action';
 import { useTranslation } from 'react-i18next';
-import { DeleteForever } from '@ndla/icons/editor';
+import { DeleteForever, DragVertical } from '@ndla/icons/editor';
 import { DraggableProvidedDragHandleProps } from 'react-beautiful-dnd';
 import { GQLFolder } from '../../../graphqlTypes';
 import { FolderTotalCount } from '../../../util/folderHelpers';
-import { FolderAction, ListItem, ViewType } from './FoldersPage';
+import { FolderAction, ViewType } from './FoldersPage';
+import { IconButtonV2 } from '@ndla/button';
+import styled from '@emotion/styled';
 
 interface DraggableFolderProps {
   id: string;
@@ -24,6 +26,19 @@ interface DraggableFolderProps {
   index: number;
   dragHandleProps?: DraggableProvidedDragHandleProps;
 }
+
+const DragButton = styled(IconButtonV2)``;
+
+const ListItem = styled.li`
+  list-style: none;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+`;
+
+const FolderWrapper = styled.div`
+  flex-grow: 1;
+`;
 
 const DraggableFolder = ({
   index,
@@ -38,30 +53,41 @@ const DraggableFolder = ({
     <ListItem
       key={`folder-${folder.id}`}
       id={`folder-${folder.id}`}
-      tabIndex={-1}
-      {...dragHandleProps}>
-      <Folder
-        key={folder.id}
-        id={folder.id}
-        link={`/minndla/folders/${folder.id}`}
-        title={folder.name}
-        type={type === 'block' ? 'block' : 'list'}
-        subFolders={foldersCount[folder.id]?.folders}
-        subResources={foldersCount[folder.id]?.resources}
-        menuItems={[
-          {
-            icon: <Pencil />,
-            text: t('myNdla.folder.edit'),
-            onClick: () => setFolderAction({ action: 'edit', folder, index }),
-          },
-          {
-            icon: <DeleteForever />,
-            text: t('myNdla.folder.delete'),
-            onClick: () => setFolderAction({ action: 'delete', folder, index }),
-            type: 'danger',
-          },
-        ]}
-      />
+      tabIndex={-1}>
+      <DragButton
+        {...dragHandleProps}
+        aria-label={'TODO'}
+        type={'button'}
+        variant={'ghost'}
+        colorTheme={'light'}
+        size={'small'}>
+        <DragVertical />
+      </DragButton>
+      <FolderWrapper>
+        <Folder
+          key={folder.id}
+          id={folder.id}
+          link={`/minndla/folders/${folder.id}`}
+          title={folder.name}
+          type={type === 'block' ? 'block' : 'list'}
+          subFolders={foldersCount[folder.id]?.folders}
+          subResources={foldersCount[folder.id]?.resources}
+          menuItems={[
+            {
+              icon: <Pencil />,
+              text: t('myNdla.folder.edit'),
+              onClick: () => setFolderAction({ action: 'edit', folder, index }),
+            },
+            {
+              icon: <DeleteForever />,
+              text: t('myNdla.folder.delete'),
+              onClick: () =>
+                setFolderAction({ action: 'delete', folder, index }),
+              type: 'danger',
+            },
+          ]}
+        />
+      </FolderWrapper>
     </ListItem>
   );
 };
