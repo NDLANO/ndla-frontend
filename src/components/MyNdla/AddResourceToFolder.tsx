56397d494281ad8052d8742dff363f3bc92c7b4b
Henrik Liodden <henrik.liodden@knowit.no>
2022-10-12 11:16:21 +0200

    Remove unnecessary maxlevel prop in treestructure

--- src/components/MyNdla/AddResourceToFolder.tsx
+++ src/components/MyNdla/AddResourceToFolder.tsx
@@ -262,7 +262,6 @@ const AddResourceToFolder = ({
       />
       <ComboboxContainer>
         <TreeStructure
-          maxLevel={5}
           folders={structureFolders}
           label={t('myNdla.myFolders')}
           onSelectFolder={setSelectedFolderId}
