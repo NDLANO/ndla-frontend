8813e76094c1ec12335a61f8197049ed978f05df
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-11-10 15:01:56 +0100

    Add copyright

--- scripts/start-ssr.ts
+++ scripts/start-ssr.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree. *
+ */
+
 import webpack, { Configuration } from 'webpack';
 import nodemon from 'nodemon';
 import express from 'express';
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree. *
+ */
+
 import { Compiler } from 'webpack';
 import chalk from 'chalk';
 
