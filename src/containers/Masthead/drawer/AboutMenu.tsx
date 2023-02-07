c3b2de6a3905cbfb6fd691a3dd305f8511d6feaf
mapoken <larssoerlie96@gmail.com>
2023-02-07 13:59:30 +0100

    Added new about links and submenu to aboutmenu

--- src/containers/Masthead/drawer/AboutMenu.tsx
+++ src/containers/Masthead/drawer/AboutMenu.tsx
@@ -6,14 +6,20 @@
  *
  */
 
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { aboutNdlaLinks, aboutNdlaUrl } from '../../../constants';
+import {
+  aboutNdlaLinkGroups,
+  aboutNdlaLinks,
+  aboutNdlaUrl,
+} from '../../../constants';
 import BackButton from './BackButton';
 import DrawerMenuItem from './DrawerMenuItem';
 import DrawerPortion, { DrawerList } from './DrawerPortion';
 import DrawerRowHeader from './DrawerRowHeader';
 import useArrowNavigation from './useArrowNavigation';
 
+export type AboutSubType = 'whatWeDo' | 'whoAreWe' | 'careers' | 'contactUs';
 interface Props {
   onClose: () => void;
   onCloseMenuPortion: () => void;
@@ -22,29 +28,54 @@ interface Props {
 const AboutMenu = ({ onClose, onCloseMenuPortion }: Props) => {
   const { t } = useTranslation();
   useArrowNavigation(true, 'header-about-ndla', undefined, onCloseMenuPortion);
+  const [subType, setSubType] = useState<AboutSubType | undefined>(undefined);
 
   return (
-    <DrawerPortion>
-      <BackButton
-        title={t('masthead.menu.goToMainMenu')}
-        homeButton
-        onGoBack={onCloseMenuPortion}
-      />
-      <DrawerList id="about-menu">
-        <DrawerRowHeader
-          id={'about-ndla'}
-          title={t('masthead.menuOptions.about.title')}
-          type="link"
-          to={aboutNdlaUrl}
-          onClose={onClose}
+    <>
+      <DrawerPortion>
+        <BackButton
+          title={t('masthead.menu.goToMainMenu')}
+          homeButton
+          onGoBack={onCloseMenuPortion}
         />
-        {Object.entries(aboutNdlaLinks).map(([key, url]) => (
-          <DrawerMenuItem key={key} id={key} type="link" to={url}>
-            {t(`masthead.menuOptions.about.${key}`)}
-          </DrawerMenuItem>
-        ))}
-      </DrawerList>
-    </DrawerPortion>
+        <DrawerList id="about-menu">
+          <DrawerRowHeader
+            id={'about-ndla'}
+            title={t('masthead.menuOptions.about.title')}
+            type="link"
+            to={aboutNdlaUrl}
+            onClose={onClose}
+          />
+          {Object.entries(aboutNdlaLinks).map(([key]) => (
+            <DrawerMenuItem
+              key={key}
+              id={key}
+              type="button"
+              onClick={() => setSubType(key as AboutSubType)}>
+              {t(`masthead.menuOptions.about.${key}`)}
+            </DrawerMenuItem>
+          ))}
+        </DrawerList>
+      </DrawerPortion>
+      {subType && (
+        <DrawerPortion>
+          <DrawerList id={'about-sub-menu'}>
+            <DrawerRowHeader
+              id={'about-sub-ndla'}
+              title={t(`masthead.menuOptions.about.${subType}`)}
+              type="link"
+              to={aboutNdlaLinks[subType]}
+              onClose={() => {}}
+            />
+          </DrawerList>
+          {Object.entries(aboutNdlaLinkGroups[subType]).map(([key, url]) => (
+            <DrawerMenuItem key={key} id={key} type="link" to={url}>
+              {t(`masthead.menuOptions.about.${key}`)}
+            </DrawerMenuItem>
+          ))}
+        </DrawerPortion>
+      )}
+    </>
   );
 };
 
