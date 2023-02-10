845a12affc7a0b403185a5623c66d5ebd498c5d9
mapoken <larssoerlie96@gmail.com>
2023-02-10 12:25:27 +0100

    Added Âforgotten license

--- src/containers/Masthead/drawer/AboutSubMenu.tsx
+++ src/containers/Masthead/drawer/AboutSubMenu.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2023-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { useTranslation } from 'react-i18next';
 import { aboutNdlaLinkGroups, aboutNdlaLinks } from '../../../constants';
 import DrawerMenuItem from './DrawerMenuItem';
