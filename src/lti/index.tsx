e31c1801ab55af3c735657db716b2eee96d9486e
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-02-21 15:17:05 +0100

    Properly handle iframe end lti client entry points

--- src/lti/index.tsx
+++ src/lti/index.tsx
@@ -8,6 +8,7 @@
 
 import 'isomorphic-unfetch';
 import ReactDOM from 'react-dom';
+import qs from 'query-string';
 import { HelmetProvider } from 'react-helmet-async';
 import { I18nextProvider } from 'react-i18next';
 import ErrorReporter from '@ndla/error-reporter';
@@ -33,12 +34,19 @@ import LtiProvider from './LtiProvider';
 import '../style/index.css';
 import { STORED_LANGUAGE_COOKIE_KEY } from '../constants';
 import { initializeI18n, isValidLocale } from '../i18n';
+import { ArticleConverterProvider } from '../components/ArticleConverterContext';
 
 const {
   DATA: { initialProps, config },
 } = window;
 
 const { logglyApiKey, logEnvironment: environment, componentName } = config;
+
+const { disableConverter } = qs.parse(window.location.search);
+
+const disableConverterValue = disableConverter?.length
+  ? disableConverter === 'true'
+  : config.disableConverter;
 
 window.errorReporter = ErrorReporter.getInstance({
   logglyApiKey,
@@ -55,15 +63,17 @@ const client = createApolloClient(language);
 const i18n = initializeI18n(i18nInstance, language);
 
 ReactDOM.render(
-  <HelmetProvider>
-    <I18nextProvider i18n={i18n}>
-      <ApolloProvider client={client}>
-        <MissingRouterContext.Provider value={true}>
-          <LtiProvider {...initialProps} />
-        </MissingRouterContext.Provider>
-      </ApolloProvider>
-    </I18nextProvider>
-  </HelmetProvider>,
+  <ArticleConverterProvider value={disableConverterValue}>
+    <HelmetProvider>
+      <I18nextProvider i18n={i18n}>
+        <ApolloProvider client={client}>
+          <MissingRouterContext.Provider value={true}>
+            <LtiProvider {...initialProps} />
+          </MissingRouterContext.Provider>
+        </ApolloProvider>
+      </I18nextProvider>
+    </HelmetProvider>
+  </ArticleConverterProvider>,
   document.getElementById('root'),
 );
 
