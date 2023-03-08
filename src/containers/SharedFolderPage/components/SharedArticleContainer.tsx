bce0cbc689368d76d63d1abfd9dc6f151e6e94e6
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-03-08 15:07:46 +0100

    Add missing copyright

--- src/containers/SharedFolderPage/components/SharedArticleContainer.tsx
+++ src/containers/SharedFolderPage/components/SharedArticleContainer.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2023-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { gql } from '@apollo/client';
 import { OneColumn } from '@ndla/ui';
 import { useEffect, useMemo } from 'react';
