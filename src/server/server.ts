9995763ee15090fea490d360ca7e549ae3c2f6e9
Jonas Carlsen <jonas.carlsen@knowit.no>
2023-03-17 13:13:22 +0100

    Update src/server/server.ts
    Co-authored-by: Gunnar Velle <gunnar.velle@gmail.com>

--- src/server/server.ts
+++ src/server/server.ts
@@ -73,9 +73,7 @@ const ndlaMiddleware = [
       allowedBodyContentTypes.includes(req.headers['content-type'] ?? ''),
   }),
   helmet({
-    crossOriginEmbedderPolicy: {
-      policy: 'credentialless',
-    },
+    crossOriginEmbedderPolicy: false,
     hsts: {
       maxAge: 31536000,
       includeSubDomains: true,
