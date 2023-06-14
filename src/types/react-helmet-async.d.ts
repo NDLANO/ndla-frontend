eb061e8d286e646ddd5dad80f93b94b844577e0b
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-06-14 12:22:31 +0200

    Add license text to react-helmet type override

--- src/types/react-helmet-async.d.ts
+++ src/types/react-helmet-async.d.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2023-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { ReactNode } from 'react';
 
 declare module 'react-helmet-async' {
