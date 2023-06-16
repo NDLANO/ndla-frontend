aa174f52b348d0857bedae8d8fab0f6d80f47d6d
Gunnar Velle <gunnar.velle@gmail.com>
2023-06-16 11:50:47 +0200

    Make border tertiary

--- src/containers/ResourceEmbed/components/ResourceEmbed.tsx
+++ src/containers/ResourceEmbed/components/ResourceEmbed.tsx
@@ -42,7 +42,7 @@ const CreatedByWrapper = styled.div`
 
 const StyledAccordionHeader = styled(AccordionHeader)`
   background-color: ${colors.brand.lightest};
-  border: 1px solid ${colors.brand.lighter};
+  border: 1px solid ${colors.brand.tertiary};
   font-size: ${fonts.sizes('16px', '29px')};
   font-weight: ${fonts.weight.semibold};
 `;
