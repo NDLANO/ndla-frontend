80f6ced9a8809d75a322700e2ba7ce9c14691c61
Gunnar Velle <gunnar.velle@gmail.com>
2023-10-09 10:22:03 +0200

    Hotfix: lang on ai-link

--- src/containers/MyNdla/MyNdlaPage.tsx
+++ src/containers/MyNdla/MyNdlaPage.tsx
@@ -143,6 +143,8 @@ const MyNdlaPage = () => {
 
   const keyedData = keyBy(metaData ?? [], (r) => `${r.type}${r.id}`);
 
+  const aiLang = i18n.language === 'nn' ? i18n.language : 'nb';
+
   return (
     <StyledPageContentContainer>
       <HelmetWithTracker title={t('htmlTitles.myNdlaPage')} />
@@ -153,7 +155,7 @@ const MyNdlaPage = () => {
       <StyledDescription>{t('myNdla.myPage.welcome')}</StyledDescription>
       {tempAllowedAIOrgs().includes(user?.baseOrg?.displayName ?? '') && (
         <StyledBannerCard
-          link="https://ai.ndla.no/"
+          link={`https://ai.ndla.no/${aiLang}`}
           title={{
             title: t('myndla.campaignBlock.title'),
             lang: i18n.language,
