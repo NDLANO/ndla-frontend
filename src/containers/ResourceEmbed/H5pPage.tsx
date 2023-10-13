3a91323eca2705b44507aa3fb68df1fb0b675d8d
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-10-13 14:31:45 +0200

    Link to /h5p/ in articles, and h5p domain on /h5p/

--- src/containers/ResourceEmbed/H5pPage.tsx
+++ src/containers/ResourceEmbed/H5pPage.tsx
@@ -4,6 +4,7 @@
  * This source code is licensed under the GPLv3 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 import { useParams } from 'react-router-dom';
 import ResourceEmbed from './components/ResourceEmbed';
 import NotFound from '../NotFoundPage/NotFoundPage';
