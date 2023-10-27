44836a26549fa35357a1ab5c2134c1960d022fa2
DeividasSvaikauskas <deividas.svaikauskas@bouvet.no>
2023-10-27 12:08:30 +0200

    imported graphql types and created avatar component

---
+++ src/containers/MyNdla/components/Avatar.tsx
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2023-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+function Avatar() {
+  return <div>Avatar</div>;
+}
+
+export default Avatar;
