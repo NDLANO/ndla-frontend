05dcadd69b69b5fc12fbe99959a89c1ce29ba1eb
John-H-Christiansen <john.christiansen@bouvet.no>
2023-12-01 12:05:25 +0100

    fixed text decoration on topic card

--- src/containers/MyNdla/Arena/components/ArenaCard.tsx
+++ src/containers/MyNdla/Arena/components/ArenaCard.tsx
@@ -40,7 +40,6 @@ const StyledCategoryCard = css`
 
 const StyledSafelink = styled(SafeLink)`
   color: ${colors.text.primary};
-
   display: flex;
   flex-direction: row;
   gap: ${spacing.normal};
@@ -49,7 +48,6 @@ const StyledSafelink = styled(SafeLink)`
   border: 1px solid ${colors.brand.light};
   border-radius: ${misc.borderRadius};
   box-shadow: none;
-
   &:hover,
   &:focus-visible {
     background-color: ${colors.brand.lighter};
--- src/containers/MyNdla/Arena/components/TopicCard.tsx
+++ src/containers/MyNdla/Arena/components/TopicCard.tsx
@@ -31,11 +31,12 @@ const StyledSafelink = styled(SafeLink)`
   border: 1px solid ${colors.brand.light};
   border-radius: ${misc.borderRadius};
   box-shadow: none;
-
   &:hover,
   &:focus-visible {
     background-color: ${colors.brand.lighter};
-    text-decoration: none;
+    [data-name='hover'] {
+      text-decoration: none;
+    }
   }
 `;
 
@@ -77,7 +78,6 @@ const LockedIconCSS = css`
 
 const TopicCard = ({ id, title, locked, timestamp, count }: Props) => {
   const { t, i18n } = useTranslation();
-
   return (
     <StyledSafelink
       id={id}
@@ -85,7 +85,12 @@ const TopicCard = ({ id, title, locked, timestamp, count }: Props) => {
       to={`/minndla/arena/topic/${id}`}
     >
       <div>
-        <StyledHeader element="label" textStyle="label-small" margin="none">
+        <StyledHeader
+          element="label"
+          textStyle="label-small"
+          margin="none"
+          data-name="hover"
+        >
           {title}
         </StyledHeader>
         <StyledText element="p" textStyle="meta-text-small" margin="none">
