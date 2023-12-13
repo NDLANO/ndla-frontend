4d0b0a2fc6ed96c0d103731c9f98362aafdc8376
Gaute Rauboti <gaute.rauboti@knowit.no>
2023-12-13 08:24:34 +0100

    Removed some buttons from the preview page on mobile

--- src/containers/MyNdla/Folders/PreviewFoldersPage.tsx
+++ src/containers/MyNdla/Folders/PreviewFoldersPage.tsx
@@ -6,69 +6,24 @@
  *
  */
 
-import { useCallback, useContext, useMemo, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import FolderActions from './FolderActions';
-import FolderButtons from './FolderButtons';
+import { useCallback, useState } from 'react';
 import { ViewType } from './FoldersPage';
-import { AuthContext } from '../../../components/AuthenticationContext';
 import { STORED_RESOURCE_VIEW_SETTINGS } from '../../../constants';
-import { GQLFolder, GQLFoldersPageQuery } from '../../../graphqlTypes';
-import { useGraphQuery } from '../../../util/runQueries';
 import SharedFolderPage from '../../SharedFolderPage/SharedFolderPage';
 import MyNdlaPageWrapper from '../components/MyNdlaPageWrapper';
-import { foldersPageQuery, useFolder } from '../folderMutations';
 
 const PreviewFoldersPage = () => {
-  const { folderId } = useParams();
-  const { examLock } = useContext(AuthContext);
-  const selectedFolder = useFolder(folderId);
   const [viewType, _setViewType] = useState<ViewType>(
     (localStorage.getItem(STORED_RESOURCE_VIEW_SETTINGS) as ViewType) || 'list',
   );
-  const { data } = useGraphQuery<GQLFoldersPageQuery>(foldersPageQuery);
-
-  const folders: GQLFolder[] = useMemo(
-    () =>
-      selectedFolder
-        ? selectedFolder.subfolders
-        : (data?.folders as GQLFolder[]) ?? [],
-    [selectedFolder, data?.folders],
-  );
-  const [_focusId, setFocusId] = useState<string | undefined>(undefined);
 
   const setViewType = useCallback((type: ViewType) => {
     _setViewType(type);
     localStorage.setItem(STORED_RESOURCE_VIEW_SETTINGS, type);
   }, []);
 
-  const dropDownMenu = useMemo(
-    () => (
-      <FolderActions
-        selectedFolder={selectedFolder}
-        setFocusId={setFocusId}
-        folders={folders}
-        inToolbar
-      />
-    ),
-    [selectedFolder, folders, setFocusId],
-  );
-
-  const folderButtons = useMemo(
-    () => (
-      <FolderButtons selectedFolder={selectedFolder} setFocusId={setFocusId} />
-    ),
-    [selectedFolder, setFocusId],
-  );
-
   return (
-    <MyNdlaPageWrapper
-      dropDownMenu={dropDownMenu}
-      buttons={folderButtons}
-      viewType={viewType}
-      onViewTypeChange={setViewType}
-      showButtons={!examLock || !!selectedFolder}
-    >
+    <MyNdlaPageWrapper viewType={viewType} onViewTypeChange={setViewType}>
       <SharedFolderPage />
     </MyNdlaPageWrapper>
   );
