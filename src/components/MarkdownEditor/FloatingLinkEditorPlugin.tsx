32cdc3a4a11f32e857c416c329e76563978eeeb0
Gaute Rauboti <gaute.rauboti@knowit.no>
2024-01-19 13:54:41 +0100

    Last popover

--- src/components/MarkdownEditor/FloatingLinkEditorPlugin.tsx
+++ src/components/MarkdownEditor/FloatingLinkEditorPlugin.tsx
@@ -38,7 +38,7 @@ import { $isLinkNode, TOGGLE_LINK_COMMAND } from '@lexical/link';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { mergeRegister, $findMatchingParent } from '@lexical/utils';
 import { ButtonV2 } from '@ndla/button';
-import { colors, misc, shadows, spacing } from '@ndla/core';
+import { colors, misc, shadows, spacing, stackOrder } from '@ndla/core';
 import { FieldErrorMessage, FormControl, InputV3, Label } from '@ndla/forms';
 import { getSelectedNode } from './EditorToolbar';
 
@@ -49,7 +49,7 @@ export const ADD_LINK_COMMAND: LexicalCommand<null> = createCommand();
 
 const FloatingContainer = styled.div`
   position: absolute;
-  z-index: 1000;
+  z-index: ${stackOrder.popover};
   display: none;
   align-items: center;
   gap: ${spacing.small};
