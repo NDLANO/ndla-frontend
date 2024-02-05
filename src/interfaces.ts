3c258d046e2108e1095ae5b28b1c0ac7726a347c
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-02-05 15:39:36 +0100

    Remove unused resCookie

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -15,7 +15,6 @@ export type InitialProps = {
   isOembed?: string;
   status?: "success" | "error";
   loading?: boolean;
-  resCookie?: string;
   basename?: string;
   locale?: LocaleType;
   ltiData?: LtiData;
@@ -24,7 +23,6 @@ export type InitialProps = {
 export interface WindowData {
   apolloState: NormalizedCacheObject;
   config: ConfigType;
-  resCookie?: string;
   initialProps: InitialProps;
   ltiData?: LtiData;
   serverPath?: string;
