9e4792eb62fd921a46ac5bedf72f268b506056d4
MaPoKen <larssoerlie96@gmail.com>
2024-02-06 10:07:55 +0100

    formatted with new prettier settings

--- e2e/specs/authenticated/MyNDLA.spec.ts
+++ e2e/specs/authenticated/MyNDLA.spec.ts
@@ -6,78 +6,58 @@
  *
  */
 
-import { test, expect } from '@playwright/test';
-import { mockGraphqlRoute, mockWaitResponse } from '../../apiMock';
+import { test, expect } from "@playwright/test";
+import { mockGraphqlRoute, mockWaitResponse } from "../../apiMock";
 
 test.beforeEach(async ({ page }) => {
   await mockGraphqlRoute({
     page,
     operation: [
       {
-        fixture: 'minndla_home_myndladata',
-        names: ['myNdlaData', 'recentlyUsed'],
+        fixture: "minndla_home_myndladata",
+        names: ["myNdlaData", "recentlyUsed"],
       },
       {
-        fixture: 'minndla_home_data',
-        names: [
-          'folderResourceMetaSearch',
-          'arenaRecentTopicsV2',
-          'arenaNotificationsV2',
-        ],
+        fixture: "minndla_home_data",
+        names: ["folderResourceMetaSearch", "arenaRecentTopicsV2", "arenaNotificationsV2"],
       },
       {
-        fixture: 'minndla_home_folder',
-        names: ['foldersPage'],
+        fixture: "minndla_home_folder",
+        names: ["foldersPage"],
       },
       {
-        fixture: 'minndla_home_arena',
-        names: ['arenaPage2'],
+        fixture: "minndla_home_arena",
+        names: ["arenaPage2"],
       },
     ],
   });
-  await page.goto('/minndla');
+  await page.goto("/minndla");
 });
 
-test('have recently added to folder', async ({ page }) => {
-  await expect(
-    page.getByRole('heading', { name: 'Nylig lagt til i mine mapper' }),
-  ).toBeInViewport();
-  await mockWaitResponse(page, '**/graphql-api/graphql');
-  expect(
-    await page
-      .getByRole('main')
-      .locator('section')
-      .first()
-      .getByRole('listitem')
-      .count(),
-  ).toBeGreaterThanOrEqual(1);
+test("have recently added to folder", async ({ page }) => {
+  await expect(page.getByRole("heading", { name: "Nylig lagt til i mine mapper" })).toBeInViewport();
+  await mockWaitResponse(page, "**/graphql-api/graphql");
+  expect(await page.getByRole("main").locator("section").first().getByRole("listitem").count()).toBeGreaterThanOrEqual(
+    1,
+  );
 
-  const toFolder = page.getByRole('link').getByText('Se alle mappene dine');
+  const toFolder = page.getByRole("link").getByText("Se alle mappene dine");
   await expect(toFolder).toBeInViewport();
   await toFolder.click();
-  await page.waitForURL('/minndla/folders');
-  await expect(
-    page.getByRole('heading', { name: 'Mine mapper' }),
-  ).toBeInViewport();
+  await page.waitForURL("/minndla/folders");
+  await expect(page.getByRole("heading", { name: "Mine mapper" })).toBeInViewport();
 });
 
-test('have new posts in arena', async ({ page }) => {
-  await expect(
-    page.getByRole('heading', { name: 'Nye innlegg i arena' }),
-  ).toBeInViewport();
+test("have new posts in arena", async ({ page }) => {
+  await expect(page.getByRole("heading", { name: "Nye innlegg i arena" })).toBeInViewport();
 
-  expect(
-    await page
-      .getByRole('main')
-      .locator('section')
-      .nth(1)
-      .getByRole('listitem')
-      .count(),
-  ).toBeGreaterThanOrEqual(1);
+  expect(await page.getByRole("main").locator("section").nth(1).getByRole("listitem").count()).toBeGreaterThanOrEqual(
+    1,
+  );
 
-  const toArena = page.getByRole('link').getByText('Se alle innlegg i arena');
+  const toArena = page.getByRole("link").getByText("Se alle innlegg i arena");
   await expect(toArena).toBeInViewport();
   await toArena.click();
-  await page.waitForURL('/minndla/arena');
-  await expect(page.getByRole('heading', { name: 'Arena' })).toBeInViewport();
+  await page.waitForURL("/minndla/arena");
+  await expect(page.getByRole("heading", { name: "Arena" })).toBeInViewport();
 });
