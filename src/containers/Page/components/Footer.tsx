48c505e9d5b4b8a68139e9488b48ac699735bd62
DeividasSvaikauskas <deividas.svaikauskas@bouvet.no>
2024-02-21 09:16:25 +0100

    deleted wrong import

--- src/containers/Page/components/Footer.tsx
+++ src/containers/Page/components/Footer.tsx
@@ -9,10 +9,10 @@
 import { useTranslation } from "react-i18next";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { ZendeskButton } from "@ndla/button";
 import { colors, spacing, stackOrder } from "@ndla/core";
 import { FacebookFilled, HelpCircleOutline, Instagram, LinkedIn, Email, Youtube } from "@ndla/icons/common";
 import { Footer, FooterText, EditorName } from "@ndla/ui";
-import ZendeskButton from "@ndla/zendesk";
 import config from "../../../config";
 
 const FooterTextWrapper = styled.div`
