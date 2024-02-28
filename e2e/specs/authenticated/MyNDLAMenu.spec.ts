b0c7c4d2ee9b7b62e8ae870ba2866f4c8ac42e53
MaPoKen <larssoerlie96@gmail.com>
2024-02-28 08:55:22 +0100

    E2E runs smoothly

--- e2e/specs/authenticated/MyNDLAMenu.spec.ts
+++ e2e/specs/authenticated/MyNDLAMenu.spec.ts
@@ -7,7 +7,7 @@
  */
 
 import { test, expect } from "@playwright/test";
-import { mockGraphqlRoute } from "../../apiMock";
+import { mockGraphqlRoute, mockWaitResponse } from "../../apiMock";
 
 test.beforeEach(async ({ page }) => {
   await mockGraphqlRoute({
@@ -71,6 +71,7 @@ test("can navigate to profile", async ({ page }) => {
 });
 
 test("have all options at the different pages", async ({ page }) => {
+  await mockWaitResponse(page, "**/graphql-api/graphql");
   await expect(page.getByRole("heading").getByText("Min NDLA")).toBeVisible();
   const options = await page.getByTestId("my-ndla-menu").getByRole("listitem").allInnerTexts();
   await page.getByRole("listitem").getByRole("link", { name: "Mine mapper" }).click();
