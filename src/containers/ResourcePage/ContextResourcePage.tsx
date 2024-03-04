edbfd5df25890caf6064dcdd50e053d30bf0f76e
Jonas Natten <jonas.natten@knowit.no>
2024-03-04 11:39:51 +0100

    mig mig mig

--- src/containers/ResourcePage/ContextResourcePage.tsx
+++ src/containers/ResourcePage/ContextResourcePage.tsx
@@ -6,6 +6,40 @@
  *
  */
 
+import { gql } from "@apollo/client";
+import { useGraphQuery } from "../../util/runQueries";
+import ArticlePage, { articlePageFragments } from "../ArticlePage/ArticlePage";
+import LearningpathPage, { learningpathPageFragments } from "../LearningpathPage/LearningpathPage";
+import MovedResourcePage from "../MovedResourcePage/MovedResourcePage";
+
+const contextResourcePageQuery = gql`
+  query contextResourcePage($path: String!) {
+    resourceByPath(path: $path) {
+      relevanceId
+      paths
+      ...MovedResourcePage_Resource
+      ...ArticlePage_Resource
+      ...LearningpathPage_Resource
+    }
+  }
+  ${articlePageFragments.topic}
+  ${MovedResourcePage.fragments.resource}
+  ${articlePageFragments.resource}
+  ${articlePageFragments.resourceType}
+  ${articlePageFragments.subject}
+  ${articlePageFragments.topicPath}
+  ${learningpathPageFragments.topic}
+  ${learningpathPageFragments.resourceType}
+  ${learningpathPageFragments.resource}
+  ${learningpathPageFragments.subject}
+  ${learningpathPageFragments.topicPath}
+`;
+
 export const ContextResourcePage = () => {
-  return <p>Yo</p>;
+  const { data } = useGraphQuery(contextResourcePageQuery, {
+    variables: {
+      path: "/how-to-make-a-good-presentation__3282c6cceeff",
+    },
+  });
+  return <pre>{JSON.stringify(data)}</pre>;
 };
