b4d4b75f78aad87170b6796e8552b8831b59beac
Katrine Wist <katrinew0702@gmail.com>
2024-08-07 15:24:29 +0200

    Search page to new design  (#1942)
    * Rewrote search page to new design
    
    * Refactor emotion styling to panda
    
    * Use new primitives components

--- src/messages/messagesNB.ts
+++ src/messages/messagesNB.ts
@@ -41,7 +41,6 @@ const messages = {
     tipsAndAdvice: "Tips og råd",
     goBack: "Gå tilbake",
   },
-  searchPage: { subjectLetter: "Fag som starter på {{letter}}" },
   podcastPage: {
     episodes: "Episoder",
     podcast: "Podkast",
@@ -51,6 +50,11 @@ const messages = {
     subtitle: "Lytt og lær!",
   },
   subjectsPage: { tabFilter: "Hvilke fag vil du vise?" },
+  searchPage: {
+    title: "Søk på ndla.no",
+    filterSearch: "Filtrer søket ditt:",
+    subjectLetter: "Fag som starter på {{letter}}",
+  },
   myndla: {
     campaignBlock: {
       title: "Prøv vår praterobot",
