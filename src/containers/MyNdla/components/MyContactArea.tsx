ab1daaf1bd9a6e11708a60be879c8357ecfe8e5e
Gaute Rauboti <gaute.rauboti@knowit.no>
2024-08-23 10:10:49 +0200

    Moved MyContactArea from PR # 1963

--- src/containers/MyNdla/components/MyContactArea.tsx
+++ src/containers/MyNdla/components/MyContactArea.tsx
@@ -6,9 +6,8 @@
  *
  */
 
-import styled from "@emotion/styled";
-import { colors, spacing, breakpoints, mq } from "@ndla/core";
-import { Heading } from "@ndla/typography";
+import { Text, Heading } from "@ndla/primitives";
+import { styled } from "@ndla/styled-system/jsx";
 import UserAvatar from "./UserAvatar";
 import { isStudent, withRole } from "../Folders/util";
 import EditProfilePicture from "../MyProfile/components/EditProfilePicture";
@@ -25,42 +24,42 @@ type MyContractAreaProps = {
   showProfileButton?: boolean;
 };
 
-const MyContactAreaContainer = styled.div`
-  max-width: 100%;
-  max-height: fit-content;
-  padding: ${spacing.large} 0;
-  border: 1px solid ${colors.brand.lighter};
-  border-radius: ${spacing.small};
-  background-color: ${colors.background.lightBlue};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: ${spacing.small};
-`;
+const MyContactAreaContainer = styled("div", {
+  base: {
+    alignItems: "center",
+    backgroundColor: "surface.brand.1.subtle",
+    border: "1px solid",
+    borderColor: "stroke.default",
+    borderRadius: "small",
+    display: "flex",
+    flexDirection: "column",
+    gap: "xxsmall",
+    padding: "xlarge",
+  },
+});
 
-const AvatarContainer = styled.div`
-  max-width: 250px;
-  max-height: 250px;
-`;
+const AvatarContainer = styled("div", {
+  base: {
+    maxHeight: "surface.xsmall",
+    maxWidth: "surface.xsmall",
+  },
+});
 
-const UserInfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${spacing.xxsmall};
-  align-items: center;
-`;
+const MobileButtonContainer = styled("div", {
+  base: {
+    paddingBlockStart: "4xsmall",
+    display: "block",
+    tablet: {
+      display: "none",
+    },
+  },
+});
 
-const UserWorkPlaceText = styled(Heading)`
-  color: ${colors.brand.primary};
-`;
-
-const MobileButtonContainer = styled.div`
-  padding-top: ${spacing.xxsmall};
-  ${mq.range({ from: breakpoints.tablet })} {
-    display: none;
-  }
-`;
-
+const StyledText = styled(Text, {
+  base: {
+    textTransform: "uppercase",
+  },
+});
 const MyContactArea = ({ user, showProfileButton }: MyContractAreaProps) => {
   return (
     <MyContactAreaContainer>
@@ -69,14 +68,12 @@ const MyContactArea = ({ user, showProfileButton }: MyContractAreaProps) => {
           <UserAvatar userName={user.displayName} />
         </AvatarContainer>
       )}
-      <Heading element="h2" id="userName" margin="none" headingStyle="h2">
-        {user.displayName}
+      <Heading id="userName" textStyle="title.medium" asChild consumeCss>
+        <h2>{user.displayName}</h2>
       </Heading>
-      <UserInfoContainer>
-        <UserWorkPlaceText element="p" headingStyle="list-title" margin="none">
-          {user.primaryOrg}
-        </UserWorkPlaceText>
-      </UserInfoContainer>
+      <StyledText textStyle="title.small" color="stroke.hover">
+        {user.primaryOrg}
+      </StyledText>
       {showProfileButton && (
         <>
           {!isStudent(user as withRole) && (
