d35dc6814aba83bdbb95063741e30679e1751fba
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-09-12 16:49:44 +0200

    Match notifications to design

--- src/containers/MyNdla/components/NotificationPopover.tsx
+++ src/containers/MyNdla/components/NotificationPopover.tsx
@@ -6,38 +6,71 @@
  *
  */
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
-import styled from "@emotion/styled";
-import { spacing } from "@ndla/core";
-import { PopoverContent, PopoverRoot, PopoverTrigger } from "@ndla/primitives";
+import { Button, Heading, PopoverContent, PopoverRoot, PopoverTitle, PopoverTrigger } from "@ndla/primitives";
 import { SafeLinkButton } from "@ndla/safelink";
+import { styled } from "@ndla/styled-system/jsx";
 import NotificationBellButton from "./NotificationButton";
 import NotificationList from "./NotificationList";
 import { routes } from "../../../routeHelpers";
-import { useTemporaryArenaNotifications } from "../Arena/components/temporaryNodebbHooks";
+import {
+  useArenaMarkNotificationsAsRead,
+  useTemporaryArenaNotifications,
+} from "../Arena/components/temporaryNodebbHooks";
 
-const ShowAllLink = styled(SafeLinkButton)`
-  margin-top: ${spacing.small};
-  width: 100%;
-`;
+const StyledPopoverContent = styled(PopoverContent, {
+  base: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "xsmall",
+    maxWidth: "surface.medium",
+  },
+});
+
+const TitleWrapper = styled("div", {
+  base: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: "xsmall",
+  },
+});
 
 const NotificationPopover = () => {
   const { notifications } = useTemporaryArenaNotifications();
+  const { markNotificationsAsRead } = useArenaMarkNotificationsAsRead();
   const { t } = useTranslation();
   const [open, setOpen] = useState(false);
+
+  const markAllRead = useCallback(async () => {
+    const topicIdsToBeMarkedRead =
+      notifications?.items.filter(({ isRead }) => !isRead)?.map(({ topicId }) => topicId) ?? [];
+
+    await markNotificationsAsRead({
+      variables: { topicIds: topicIdsToBeMarkedRead },
+    });
+  }, [notifications, markNotificationsAsRead]);
 
   return (
     <PopoverRoot open={open} onOpenChange={(details) => setOpen(details.open)}>
       <PopoverTrigger asChild>
         <NotificationBellButton notifications={notifications?.items} />
       </PopoverTrigger>
-      <PopoverContent>
+      <StyledPopoverContent>
+        <TitleWrapper>
+          <Heading asChild consumeCss textStyle="title.medium">
+            <PopoverTitle>{t("myNdla.arena.notification.title")}</PopoverTitle>
+          </Heading>
+          <Button variant="link" onClick={markAllRead}>
+            {t("myNdla.arena.notification.markAll")}
+          </Button>
+        </TitleWrapper>
         <NotificationList notifications={notifications?.items} close={() => setOpen(false)} />
-        <ShowAllLink to={routes.myNdla.notifications} onClick={() => setOpen(false)}>
+        <SafeLinkButton to={routes.myNdla.notifications} onClick={() => setOpen(false)}>
           {t("myNdla.arena.notification.showAll")}
-        </ShowAllLink>
-      </PopoverContent>
+        </SafeLinkButton>
+      </StyledPopoverContent>
     </PopoverRoot>
   );
 };
