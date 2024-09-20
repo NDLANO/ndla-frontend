ce9e7e9a7ea6ee36669805c6897938656da6a241
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-09-20 09:22:14 +0200

    fix: only set headingId in NavigationBox if Heading is passed in

--- src/components/NavigationBox.tsx
+++ src/components/NavigationBox.tsx
@@ -52,7 +52,7 @@ export const NavigationBox = ({ heading, variant, items }: Props & NavigationSaf
   const { t } = useTranslation();
   const headingId = useId();
   return (
-    <StyledWrapper aria-labelledby={headingId} data-nav-box="">
+    <StyledWrapper aria-labelledby={heading ? headingId : undefined} data-nav-box="">
       {heading && (
         <Heading id={headingId} asChild consumeCss textStyle="heading.small" fontWeight="bold">
           <h2>{heading}</h2>
