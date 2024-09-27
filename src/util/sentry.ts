8c7abfa33516b52f46effdae78bc7c25f6c140f4
Jonas Natten <jonas.natten@knowit.no>
2024-09-27 12:52:05 +0200

    Stop initalizing sentry locally

--- src/util/sentry.ts
+++ src/util/sentry.ts
@@ -10,6 +10,11 @@ import * as Sentry from "@sentry/react";
 import { ConfigType } from "../config";
 
 export const initSentry = (config: ConfigType) => {
+  if (config.ndlaEnvironment === "local" || config.ndlaEnvironment === "dev") {
+    // Skipping sentry initialization in local and dev environments
+    return;
+  }
+
   Sentry.init({
     dsn: config.sentrydsn,
     environment: config.ndlaEnvironment,
