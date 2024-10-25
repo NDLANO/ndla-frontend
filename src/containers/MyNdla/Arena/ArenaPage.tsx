b25f26aace8de2721cf570c02a20237c1954aa8d
Gunnar Velle <gunnar.velle@gmail.com>
2024-10-25 10:35:01 +0200

    Move iframe up

--- src/containers/MyNdla/Arena/ArenaPage.tsx
+++ src/containers/MyNdla/Arena/ArenaPage.tsx
@@ -6,28 +6,15 @@
  *
  */
 
-import parse from "html-react-parser";
 import { useContext, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Navigate } from "react-router-dom";
-import { AddLine } from "@ndla/icons/action";
-import { Text, Heading } from "@ndla/primitives";
 import { styled } from "@ndla/styled-system/jsx";
 import { HelmetWithTracker, useTracker } from "@ndla/tracker";
 import { AuthContext } from "../../../components/AuthenticationContext";
-import { SKIP_TO_CONTENT_ID } from "../../../constants";
 import { routes } from "../../../routeHelpers";
 import { getAllDimensions } from "../../../util/trackingUtil";
 import MyNdlaPageWrapper from "../components/MyNdlaPageWrapper";
-import { MenuItemProps } from "../components/SettingsMenu";
-
-const HeadingWrapper = styled("div", {
-  base: {
-    display: "flex",
-    flexDirection: "column",
-    gap: "xsmall",
-  },
-});
 
 const StyledMyNdlaPageWrapper = styled(MyNdlaPageWrapper, {
   base: {
@@ -58,25 +45,9 @@ const ArenaPage = () => {
 
   if (!authenticated || (user && !user.arenaEnabled)) return <Navigate to={routes.myNdla.root} />;
 
-  const menuItems: MenuItemProps[] = [
-    {
-      type: "link",
-      value: "newCategory",
-      icon: <AddLine size="small" />,
-      text: t("myNdla.arena.admin.category.form.newCategory"),
-      link: "category/new",
-    },
-  ];
-
   return (
-    <StyledMyNdlaPageWrapper menuItems={user?.isModerator ? menuItems : []}>
+    <StyledMyNdlaPageWrapper menuItems={[]}>
       <HelmetWithTracker title={t("htmlTitles.arenaPage")} />
-      <HeadingWrapper>
-        <Heading id={SKIP_TO_CONTENT_ID} textStyle="heading.medium">
-          {t("myNdla.arena.title")}
-        </Heading>
-        <Text textStyle="body.xlarge">{parse(t("myNdla.arena.notification.description"))}</Text>
-      </HeadingWrapper>
       <iframe title="Arena" src="https://grupper.test.ndla.no" width="100%" height="1000" allowFullScreen />
     </StyledMyNdlaPageWrapper>
   );
