19e4b8926537968062ad8b1aaee45debe3041864
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-11-22 13:28:41 +0100

    fix: horizontally center spinner. Wrap it in div to avoid extra padding

--- src/containers/Resources/Resources.tsx
+++ src/containers/Resources/Resources.tsx
@@ -95,6 +95,14 @@ const ResourceContainer = styled("div", {
   },
 });
 
+const SpinnerWrapper = styled("div", {
+  base: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
 const Resources = ({
   parentId,
   rootId,
@@ -159,7 +167,11 @@ const Resources = ({
   }, [node?.children]);
 
   if (loading) {
-    return <Spinner />;
+    return (
+      <SpinnerWrapper>
+        <Spinner />
+      </SpinnerWrapper>
+    );
   }
 
   if (error || !sortedResources.length) {
