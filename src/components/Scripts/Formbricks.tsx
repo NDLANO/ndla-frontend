dd22042e036e44f4527b3945757857baf9855a90
Jonas Natten <jonas.natten@knowit.no>
2024-11-22 10:41:07 +0100

    Attempt to fix formbricks init error

--- src/components/Scripts/Formbricks.tsx
+++ src/components/Scripts/Formbricks.tsx
@@ -17,7 +17,7 @@ const Formbricks = () => {
       type="text/javascript"
       dangerouslySetInnerHTML={{
         __html: `
-    !function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://app.formbricks.com/api/packages/website";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e),setTimeout(function(){window.formbricks.init({environmentId: "${config.formbricksId}",  apiHost: "https://app.formbricks.com"})},500)}();
+    !function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://app.formbricks.com/api/packages/website";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e),setTimeout(function(){window.formbricks?.init({environmentId: "${config.formbricksId}",  apiHost: "https://app.formbricks.com"})},500)}();
 `,
       }}
     />
