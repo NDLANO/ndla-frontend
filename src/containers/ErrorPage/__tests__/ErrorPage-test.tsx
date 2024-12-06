ef3ba56ec048825ef8e7e27f64a249164c007f48
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-12-06 16:20:10 +0100

    feat: upgrade to react 19

--- src/containers/ErrorPage/__tests__/ErrorPage-test.tsx
+++ src/containers/ErrorPage/__tests__/ErrorPage-test.tsx
@@ -6,15 +6,12 @@
  *
  */
 
-import { HelmetProvider } from "react-helmet-async";
 import { I18nextProvider } from "react-i18next";
 import { StaticRouter } from "react-router-dom/server.js";
 import { render } from "@testing-library/react";
 import { i18nInstance } from "@ndla/ui";
 import { initializeI18n } from "../../../i18n";
 import ErrorPage from "../ErrorPage";
-
-HelmetProvider.canUseDOM = false;
 
 vi.mock("../../../config.ts", () => {
   return {
@@ -28,13 +25,11 @@ vi.mock("../../../config.ts", () => {
 test("ErrorPage renderers correctly", () => {
   const i18n = initializeI18n(i18nInstance, "nb");
   const { asFragment } = render(
-    <HelmetProvider>
-      <I18nextProvider i18n={i18n}>
-        <StaticRouter location="">
-          <ErrorPage />
-        </StaticRouter>
-      </I18nextProvider>
-    </HelmetProvider>,
+    <I18nextProvider i18n={i18n}>
+      <StaticRouter location="">
+        <ErrorPage />
+      </StaticRouter>
+    </I18nextProvider>,
   );
 
   expect(asFragment()).toMatchSnapshot();
