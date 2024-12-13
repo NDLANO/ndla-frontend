0e7d465748f98774de84f315db2489ba11ceb045
Gunnar Velle <gunnar.velle@gmail.com>
2024-12-13 10:25:58 +0100

    Early return for learningpaths

--- src/server/routes/oembedArticleRoute.ts
+++ src/server/routes/oembedArticleRoute.ts
@@ -15,10 +15,9 @@ import config from "../../config";
 import { fetchArticle } from "../../containers/ArticlePage/articleApi";
 import { getArticleIdFromResource } from "../../containers/Resources/resourceHelpers";
 import { GQLEmbedOembedQuery, GQLEmbedOembedQueryVariables } from "../../graphqlTypes";
-import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../../statusCodes";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND } from "../../statusCodes";
 import { apiResourceUrl, createApolloClient, resolveJsonOrRejectWithError } from "../../util/apiHelpers";
 import handleError from "../../util/handleError";
-import { StatusError } from "../../util/error/StatusError";
 import { parseOembedUrl } from "../../util/urlHelper";
 
 const baseUrl = apiResourceUrl("/taxonomy/v1");
@@ -72,11 +71,10 @@ const getHTMLandTitle = async (match: PathMatch<MatchParams>, req: express.Reque
   const width = req.query.width || 854;
   const nodeId = topicId && !resourceId ? `urn:topic${topicId}` : `urn:resource${resourceId}`;
   const node = contextId ? await queryNodeByContexts(contextId, lang) : await fetchNode(nodeId, lang);
-  const articleId = getArticleIdFromResource(node);
-  if (!articleId && node.contentUri?.includes("learningpath")) {
-    throw new StatusError("No oembed support for learningpaths", 400, {});
+  if (node.contentUri?.includes("learningpath")) {
+    return {};
   }
-
+  const articleId = getArticleIdFromResource(node);
   return {
     title: node.name,
     html: `<iframe aria-label="${node.name}" src="${config.ndlaFrontendDomain}/article-iframe/${lang}/${node.id}/${articleId}" height="${height}" width="${width}" frameborder="0" allowFullscreen="" />`,
@@ -185,6 +183,12 @@ export async function oembedArticleRoute(req: express.Request) {
       return getOembedObject(req, article.title.title, html);
     }
     const { html, title } = await getHTMLandTitle(match, req);
+    if (!html && !title) {
+      return {
+        status: NOT_FOUND,
+        data: "Not found",
+      };
+    }
     return getOembedObject(req, title, html);
   } catch (error) {
     handleError(error, req.path);
