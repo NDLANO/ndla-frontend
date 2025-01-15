8b4c075451dcac559f6fe2b63520c6d96dac8181
Gunnar Velle <gunnar.velle@gmail.com>
2025-01-15 15:25:54 +0100

    Log error

--- src/server/api.ts
+++ src/server/api.ts
@@ -146,7 +146,7 @@ router.get("/login/success", async (req, res) => {
       res.cookie("nodebb_auth", nodebbCookieString, { expires: new Date(feideCookie.ndla_expires_at), domain });
     }
   } catch (error) {
-    // No cookie for you
+    log.error("Failed to set cookie for nodebb autologin", { error });
   }
 
   const languageCookie = getCookie(STORED_LANGUAGE_COOKIE_KEY, req.headers.cookie ?? "");
