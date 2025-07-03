92fc0e2e82d479eb274b7a5d7bf8b6d76329c0f7
Jonas Natten <jonas.natten@knowit.no>
2025-07-03 13:59:26 +0200

    fix: return 404 error on 404 on `PlainArticlePage`

--- src/containers/PlainArticlePage/PlainArticlePage.tsx
+++ src/containers/PlainArticlePage/PlainArticlePage.tsx
@@ -17,7 +17,7 @@ import ResponseContext from "../../components/ResponseContext";
 import { SKIP_TO_CONTENT_ID } from "../../constants";
 import { GQLPlainArticlePageQuery, GQLPlainArticlePageQueryVariables } from "../../graphqlTypes";
 import { TypedParams, useTypedParams } from "../../routeHelpers";
-import { isAccessDeniedError } from "../../util/handleError";
+import { isAccessDeniedError, isNotFoundError } from "../../util/handleError";
 import { AccessDeniedPage } from "../AccessDeniedPage/AccessDeniedPage";
 import { NotFoundPage } from "../NotFoundPage/NotFoundPage";
 import { UnpublishedResourcePage } from "../UnpublishedResourcePage/UnpublishedResourcePage";
@@ -70,6 +70,9 @@ const PlainArticlePage = () => {
   if (error) {
     if (isAccessDeniedError(error)) {
       return <AccessDeniedPage />;
+    }
+    if (isNotFoundError(error)) {
+      return <NotFoundPage />;
     }
     return <DefaultErrorMessagePage />;
   }
