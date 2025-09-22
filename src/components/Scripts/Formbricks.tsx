47409b7a5770ad1477fff3a2627bbc611c2f68b0
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-09-22 10:52:06 +0200

    fix: MIME mismatch for formbricks script

--- src/components/Scripts/Formbricks.tsx
+++ src/components/Scripts/Formbricks.tsx
@@ -12,12 +12,13 @@ const Formbricks = () => {
   if (!config.formbricksId) {
     return null;
   }
+  const host = "https://app.formbricks.com";
   return (
     <script
       type="text/javascript"
       dangerouslySetInnerHTML={{
         __html: `
-    !function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://app.formbricks.com/api/packages/website";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e),setTimeout(function(){window.formbricks?.init({environmentId: "${config.formbricksId}",  apiHost: "https://app.formbricks.com"})},500)}();
+    !function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="${host}/js/formbricks.umd.cjs";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e),setTimeout(function(){window.formbricks.setup({environmentId: "${config.formbricksId}", appUrl: "${host}"})},500)}();
 `,
       }}
     />
