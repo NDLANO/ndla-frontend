2f6b8449b27d3d3d9c40c590a1ddd62819b3def2
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-11-03 15:57:11 +0100

    perf: move authenticated check further down in tree

--- src/components/FeideLoginButton/FeideLoginButton.tsx
+++ src/components/FeideLoginButton/FeideLoginButton.tsx
@@ -6,19 +6,16 @@
  *
  */
 
-import { ReactNode, useContext } from "react";
+import { useContext } from "react";
 import { useTranslation } from "react-i18next";
+import { Feide, HeartLine } from "@ndla/icons";
 import { Button, DialogRoot, DialogTrigger } from "@ndla/primitives";
 import { SafeLinkButton } from "@ndla/safelink";
 import { css } from "@ndla/styled-system/css";
+import { styled } from "@ndla/styled-system/jsx";
 import { routes } from "../../routeHelpers";
 import { AuthContext } from "../AuthenticationContext";
 import { LoginModalContent } from "../MyNdla/LoginModalContent";
-
-interface Props {
-  footer?: boolean;
-  children?: ReactNode;
-}
 
 const buttonCss = css.raw({
   tabletDown: {
@@ -26,14 +23,23 @@ const buttonCss = css.raw({
   },
 });
 
-export const FeideLoginButton = ({ children }: Props) => {
-  const { t } = useTranslation();
-  const { authenticated } = useContext(AuthContext);
+const FeideLoginLabel = styled("span", {
+  base: {
+    tabletDown: {
+      display: "none",
+    },
+  },
+});
 
-  if (authenticated) {
+export const FeideLoginButton = () => {
+  const { t } = useTranslation();
+  const { user, authenticated } = useContext(AuthContext);
+
+  if (authenticated && user) {
     return (
       <SafeLinkButton variant="tertiary" css={buttonCss} to={routes.myNdla.root} aria-label={t("myNdla.myNDLA")}>
-        {children}
+        <HeartLine />
+        <FeideLoginLabel>{t("myNdla.myNDLA")}</FeideLoginLabel>
       </SafeLinkButton>
     );
   }
@@ -42,7 +48,8 @@ export const FeideLoginButton = ({ children }: Props) => {
     <DialogRoot>
       <DialogTrigger asChild>
         <Button variant="tertiary" css={buttonCss} aria-label={t("user.buttonLogIn")} title={t("user.buttonLogIn")}>
-          {children}
+          <Feide />
+          <FeideLoginLabel>{t("login")}</FeideLoginLabel>
         </Button>
       </DialogTrigger>
       <LoginModalContent masthead />
