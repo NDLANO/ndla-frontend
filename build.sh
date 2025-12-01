f6dab9a3f0316dde559b3b411a8e8db995bb622f
Jonas Natten <jonas.natten@knowit.no>
2025-12-01 14:09:06 +0100

    feat(build): support overriding build.sh with env variables

--- build.sh
+++ build.sh
@@ -10,10 +10,14 @@ then
     VERSION="SNAPSHOT"
 fi
 
+BUILD_CMD=${DOCKER_BUILD_CMD:-docker build}
+BUILD_ARGS=${DOCKER_BUILD_ARGS:-}
+BUILD_TAG_ARGS=${DOCKER_BUILD_TAG_ARGS:---tag $PROJECT:$VERSION}
 
-docker build \
+$BUILD_CMD \
+  $BUILD_ARGS \
   --build-arg COMPONENT_VERSION=$VERSION \
   --secret id=sentry_token,env=SENTRY_AUTH_TOKEN \
-  --tag $PROJECT:$VERSION \
+  $BUILD_TAG_ARGS \
   .
 echo "BUILT $PROJECT:$VERSION"
