a54ce1f024a323e2c3bce015173dbe83e05efaf6
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-12-17 10:27:52 +0100

    fix: do not lazy load content within SettingsMenu

---
+++ src/components/MyNdla/AddResourceToFolderModalContent.tsx
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2025-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { useTranslation } from "react-i18next";
+import { DialogBody, DialogContent, DialogHeader, DialogTitle } from "@ndla/primitives";
+import AddResourceToFolder, { ResourceAttributes } from "./AddResourceToFolder";
+import { GQLFolder } from "../../graphqlTypes";
+import { DialogCloseButton } from "../DialogCloseButton";
+
+interface Props {
+  close: VoidFunction;
+  defaultOpenFolder?: GQLFolder;
+  resource: ResourceAttributes;
+}
+
+export const AddResourceToFolderModalContent = ({ resource, defaultOpenFolder, close }: Props) => {
+  const { t } = useTranslation();
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>{t("myNdla.resource.addToMyNdla")}</DialogTitle>
+        <DialogCloseButton />
+      </DialogHeader>
+      <DialogBody>
+        <AddResourceToFolder onClose={close} resource={resource} defaultOpenFolder={defaultOpenFolder} />
+      </DialogBody>
+    </DialogContent>
+  );
+};
