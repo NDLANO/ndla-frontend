7af219fa6ba94fcf55480e4317086fa4586e17b1
Jonas Carlsen <jonas.carlsen@knowit.no>
2026-01-27 07:33:20 +0100

    refactor: simplify titleHelper

--- src/util/titleHelper.ts
+++ src/util/titleHelper.ts
@@ -6,7 +6,10 @@
  *
  */
 
-export const htmlTitle = (main: string | undefined, elements?: Array<string | undefined>): string => {
-  const subs = elements?.filter((e) => e).map((e) => " - " + e) || [];
-  return `${main || ""}${subs.join("")}`;
-};
+export const htmlTitle = (main: string | undefined, elements: Array<string | undefined>): string =>
+  elements?.reduce<string>((acc, curr) => {
+    if (curr) {
+      acc += ` - ${curr}`;
+    }
+    return acc;
+  }, main ?? "");
