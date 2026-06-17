78bdd32d366c614de00bd0583b8525e5969dc024
ingridleriksen <ingrid.l.eriksen@knowit.no>
2026-06-17 14:22:39 +0200

    update @ndla/preset-panda to version 0.0.78 and refactor resource helper

--- src/server/utils/resourceHelpers.ts
+++ src/server/utils/resourceHelpers.ts
@@ -9,16 +9,16 @@
 export const URN_ARTICLE = "urn:article:";
 export const URN_LEARTNING_PATH = "urn:learningpath:";
 
-export const isLearningPathResource = (contentUri: string | null | undefined): contentUri is string =>
+export const isLearningPathResource = (contentUri: string | undefined): contentUri is string =>
   !!contentUri?.startsWith(URN_LEARTNING_PATH);
 
-export const isArticleResource = (contentUri: string | null | undefined): contentUri is string =>
+export const isArticleResource = (contentUri: string | undefined): contentUri is string =>
   !!contentUri?.startsWith(URN_ARTICLE) && contentUri.length > 12;
 
-export const getArticleIdFromResource = (contentUri: string | null | undefined) => {
+export const getArticleIdFromResource = (contentUri: string | undefined) => {
   return isArticleResource(contentUri) ? contentUri.replace(URN_ARTICLE, "") : undefined;
 };
 
-export const getLearningPathIdFromResource = (contentUri: string | null | undefined) => {
+export const getLearningPathIdFromResource = (contentUri: string | undefined) => {
   return isLearningPathResource(contentUri) ? contentUri.replace(URN_LEARTNING_PATH, "") : undefined;
 };
